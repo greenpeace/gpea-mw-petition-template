2b0ec4c12cde7724245ede13a1ff83cc79286ee2
KFC <chris.chan@greenpeace.org>
2021-12-02 17:57:29 +0800

    update

--- pages/_document.js
+++ pages/_document.js
@@ -18,7 +18,7 @@ class NextDocument extends Document {
           {/* Load google fonts */}
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@300;400;500&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@300;400;500;700&display=swap"
             rel="stylesheet"
           />
         </Head>
