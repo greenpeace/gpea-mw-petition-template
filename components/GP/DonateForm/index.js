1c469615f99e3d1054227191a3c02830ff256332
CK Lam <ckhk0621@gmail.com>
2021-12-13 16:33:54 +0800

    new message screen for donate form

--- components/GP/DonateForm/index.js
+++ components/GP/DonateForm/index.js
@@ -9,7 +9,9 @@ import {
   Divider,
   Heading,
 } from '@chakra-ui/react';
+import * as formActions from 'store/actions/action-types/form-actions';
 import StepProgress from '@components/Progress/StepProgress';
+import MessageSection from './messageSection';
 import SubmitSection from './submitSection';
 
 const DonateForm = (props) => {
@@ -24,8 +26,11 @@ const DonateForm = (props) => {
       donate_type,
     },
     theme,
+    showMessage,
+    setShowMessage,
+    donateType,
+    setDonateType,
   } = props;
-  const [donateType, setDonateType] = useState('monthly');
   const [item, setItem] = useState();
   const [amount, setAmount] = useState(default_amount);
   const [url, setURL] = useState({ type: donateType, amount: amount });
@@ -38,6 +43,7 @@ const DonateForm = (props) => {
       value === 'monthly' ? amount_monthly[0].value : amount_onetime[0].value,
     );
     setURL({ ...url, type: value });
+    setShowMessage(value === 'single');
   };
 
   useEffect(() => {
@@ -50,151 +56,157 @@ const DonateForm = (props) => {
 
   return (
     <Box>
-      <StepProgress />
-      <Box py="6" px="4">
-        <Stack spacing="4">
-          {donate_header && (
-            <Box>
-              <Heading
-                fontSize={{ base: '2xl', xl: '3xl' }}
-                fontWeight={700}
-                dangerouslySetInnerHTML={{ __html: donate_header }}
-              />
-            </Box>
-          )}
+      {showMessage && <MessageSection />}
 
-          {(donate_description || []).map((d, i) => (
-            <Box key={i}>
-              <Text
-                as="p"
-                fontSize={{ base: 'sm', xl: 'md' }}
-                dangerouslySetInnerHTML={{ __html: d }}
-              />
-            </Box>
-          ))}
+      {!showMessage && (
+        <Box>
+          <StepProgress />
+          <Box py="6" px="4">
+            <Stack spacing="4">
+              {donate_header && (
+                <Box>
+                  <Heading
+                    fontSize={{ base: '2xl', xl: '3xl' }}
+                    fontWeight={700}
+                    dangerouslySetInnerHTML={{ __html: donate_header }}
+                  />
+                </Box>
+              )}
 
-          <Box>
-            <Stack direction="row" spacing={2} overflow={'hidden'}>
-              {(donate_type || []).map((d, i) => (
-                <Box
-                  key={i}
-                  bordertype={`solid`}
-                  borderWidth={`2px`}
-                  borderColor={
-                    donateType === d.value
-                      ? `theme.${themeInterests}`
-                      : 'gray.200'
-                  }
-                  flex={1}
-                  fontWeight="400"
-                  textAlign="center"
-                  height="auto"
-                  pos="relative"
-                  padding="12px 4px"
-                  overflow="hidden"
-                  borderRadius="md"
-                  _before={beforeProps}
-                  _after={{
-                    ...afterProps,
-                    backgroundColor:
-                      donateType === d.value
-                        ? `theme.${themeInterests}`
-                        : '#fff',
-                  }}
-                  onClick={() => {
-                    d.value === 'single'
-                      ? setItem(amount_onetime[0])
-                      : setItem(amount_monthly[0]);
-                    handleSetDonateType(d.value);
-                  }}
-                  cursor={`pointer`}
-                >
-                  {d.label}
+              {(donate_description || []).map((d, i) => (
+                <Box key={i}>
+                  <Text
+                    as="p"
+                    fontSize={{ base: 'sm', xl: 'md' }}
+                    dangerouslySetInnerHTML={{ __html: d }}
+                  />
                 </Box>
               ))}
-            </Stack>
-          </Box>
 
-          <Divider />
-
-          <Box>
-            <Grid templateColumns="repeat(3, 1fr)" gap={2}>
-              {(amountOption || []).map((d, i) => {
-                const colSpan = amountOption.length === i + 1 ? 3 : 1;
-                const isOther = d.value === 'other';
-                if (item && item.value === 'other' && isOther) {
-                  return;
-                }
-                return (
-                  <GridItem colSpan={colSpan} key={i}>
+              <Box>
+                <Stack direction="row" spacing={2} overflow={'hidden'}>
+                  {(donate_type || []).map((d, i) => (
                     <Box
-                      textAlign={`center`}
-                      border={`2px`}
+                      key={i}
+                      bordertype={`solid`}
+                      borderWidth={`2px`}
                       borderColor={
-                        amount === d.value
+                        donateType === d.value
                           ? `theme.${themeInterests}`
-                          : isOther
-                          ? '#FFF'
                           : 'gray.200'
                       }
-                      borderRadius={'md'}
+                      flex={1}
                       fontWeight="400"
-                      position={'relative'}
-                      overflow={'hidden'}
-                      minH={'48px'}
-                      _before={{
-                        ...beforeProps,
-                        top: '-4px',
-                      }}
+                      textAlign="center"
+                      height="auto"
+                      pos="relative"
+                      padding="12px 4px"
+                      overflow="hidden"
+                      borderRadius="md"
+                      _before={beforeProps}
                       _after={{
                         ...afterProps,
                         backgroundColor:
-                          amount === d.value
+                          donateType === d.value
                             ? `theme.${themeInterests}`
                             : '#fff',
                       }}
                       onClick={() => {
-                        const updateItem = {
-                          ...d,
-                          description: d.description
-                            ? d.description
-                            : item.description,
-                        };
-                        setAmount(d.value);
-                        setItem(updateItem);
+                        d.value === 'single'
+                          ? setItem(amount_onetime[0])
+                          : setItem(amount_monthly[0]);
+                        handleSetDonateType(d.value);
                       }}
-                      py={2}
-                      cursor={'pointer'}
+                      cursor={`pointer`}
                     >
-                      <Text
-                        as={'span'}
-                        fontWeight={isOther ? 400 : 'bold'}
-                        fontSize={isOther ? 'sm' : '2xl'}
-                      >
-                        {d.label}
-                      </Text>
-                      <br />
-                      <Text as={'span'} fontSize="sm" fontWeight={400}>
-                        {d.currency}
-                      </Text>
+                      {d.label}
                     </Box>
-                  </GridItem>
-                );
-              })}
-            </Grid>
-            {item && (
-              <Box>
-                <SubmitSection
-                  submitType={item.value}
-                  amount={amount}
-                  donateType={donateType}
-                  description={item ? item.description : default_message}
-                />
+                  ))}
+                </Stack>
               </Box>
-            )}
+
+              <Divider />
+
+              <Box>
+                <Grid templateColumns="repeat(3, 1fr)" gap={2}>
+                  {(amountOption || []).map((d, i) => {
+                    const colSpan = amountOption.length === i + 1 ? 3 : 1;
+                    const isOther = d.value === 'other';
+                    if (item && item.value === 'other' && isOther) {
+                      return;
+                    }
+                    return (
+                      <GridItem colSpan={colSpan} key={i}>
+                        <Box
+                          textAlign={`center`}
+                          border={`2px`}
+                          borderColor={
+                            amount === d.value
+                              ? `theme.${themeInterests}`
+                              : isOther
+                              ? '#FFF'
+                              : 'gray.200'
+                          }
+                          borderRadius={'md'}
+                          fontWeight="400"
+                          position={'relative'}
+                          overflow={'hidden'}
+                          minH={'48px'}
+                          _before={{
+                            ...beforeProps,
+                            top: '-4px',
+                          }}
+                          _after={{
+                            ...afterProps,
+                            backgroundColor:
+                              amount === d.value
+                                ? `theme.${themeInterests}`
+                                : '#fff',
+                          }}
+                          onClick={() => {
+                            const updateItem = {
+                              ...d,
+                              description: d.description
+                                ? d.description
+                                : item.description,
+                            };
+                            setAmount(d.value);
+                            setItem(updateItem);
+                          }}
+                          py={2}
+                          cursor={'pointer'}
+                        >
+                          <Text
+                            as={'span'}
+                            fontWeight={isOther ? 400 : 'bold'}
+                            fontSize={isOther ? 'sm' : '2xl'}
+                          >
+                            {d.label}
+                          </Text>
+                          <br />
+                          <Text as={'span'} fontSize="sm" fontWeight={400}>
+                            {d.currency}
+                          </Text>
+                        </Box>
+                      </GridItem>
+                    );
+                  })}
+                </Grid>
+                {item && (
+                  <Box>
+                    <SubmitSection
+                      submitType={item.value}
+                      amount={amount}
+                      donateType={donateType}
+                      description={item ? item.description : default_message}
+                    />
+                  </Box>
+                )}
+              </Box>
+            </Stack>
           </Box>
-        </Stack>
-      </Box>
+        </Box>
+      )}
     </Box>
   );
 };
@@ -224,7 +236,20 @@ const mapStateToProps = ({ form, theme }) => {
   return {
     formContent: form.content,
     theme: theme.data,
+    showMessage: form.showMessage,
+    donateType: form.donateType,
   };
 };
 
-export default connect(mapStateToProps)(DonateForm);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setShowMessage: (data) => {
+      dispatch({ type: formActions.SET_SHOW_MESSAGE, data });
+    },
+    setDonateType: (data) => {
+      dispatch({ type: formActions.SET_DONATE_TYPE, data });
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(DonateForm);
