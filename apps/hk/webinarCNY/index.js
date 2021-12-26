2b0a8f234dee0b7e3f2aa3ebad9e7e3467a8348d
CK Lam <ckhk0621@gmail.com>
2021-12-26 14:17:33 +0800

    Porject init

---
+++ apps/hk/webinarCNY/index.js
@@ -0,0 +1,106 @@
+import React, { useEffect, useRef } from 'react';
+import dynamic from 'next/dynamic';
+import OverflowWrapper from '@containers/overflowWrapper';
+import ContentContainer from '@containers/contentContainer';
+import FormContainer from '@containers/formContainer';
+import PetitionFooter from '@containers/petitionFooter';
+import { useInView } from 'react-intersection-observer';
+import { connect } from 'react-redux';
+import { Box, Flex } from '@chakra-ui/react';
+import formContent from './form';
+import SEO from './SEO';
+import * as formActions from 'store/actions/action-types/form-actions';
+
+import heroBannerImage from './images/chuen_hub.jpg';
+
+const Content = dynamic(() => import('./Content'));
+const Thankyou = dynamic(() => import('./Thankyou'));
+
+const HeroBanner = dynamic(() => import('@components/Banner/hero'));
+const ThanksBanner = dynamic(() => import('@components/Banner/thanks'));
+const PageContainer = dynamic(() => import('@containers/pageContainer'));
+
+const DonateForm = dynamic(() => import('@components/GP/DonateForm'));
+const SignupForm = dynamic(() => import('@components/GP/HKForm'));
+const FixedCTA = dynamic(() => import('@components/GP/FixedCTA'));
+
+function Index({ status, theme, setFormContent, signup }) {
+  const { submitted } = status;
+  const { FirstName } = signup;
+
+  const scrollToRef = (ref) =>
+    ref.current?.scrollIntoView({ behavior: 'smooth' });
+  const { ref, inView } = useInView({
+    /* Optional options */
+    threshold: 0,
+  });
+  const myRef = useRef(null);
+  const executeScroll = () => scrollToRef(myRef);
+
+  useEffect(() => {
+    setFormContent(formContent);
+  }, []);
+
+  return (
+    <>
+      <SEO />
+      {submitted ? (
+        <ThanksBanner
+          bgImage={heroBannerImage}
+          content={{
+            title: `${
+              FirstName ? FirstName : '綠色和平支持者'
+            }，感謝您報名 綠色新春教室`,
+            description: ['為無塑環境多走一步，捐助支持走塑項目。'],
+          }}
+        />
+      ) : (
+        <HeroBanner
+          bgImage={heroBannerImage}
+          content={{
+            title: '綠色新春教室:<br/>裸買店主同你環保辦年貨與大掃除',
+            description: [''],
+          }}
+        />
+      )}
+      <PageContainer>
+        <OverflowWrapper>
+          <Flex flexDirection={{ base: 'column-reverse', md: 'row' }}>
+            <Box flex={1} mt={{ base: 10, sm: 60 }}>
+              <ContentContainer theme={theme}>
+                {submitted ? <Thankyou /> : <Content />}
+              </ContentContainer>
+            </Box>
+            <Box flex={1} ref={myRef}>
+              <FormContainer>
+                <Box ref={ref}>
+                  {submitted ? <DonateForm /> : <SignupForm showProcess={false} showBirthdate={false} showNewletter={false}/>}
+                </Box>
+              </FormContainer>
+            </Box>
+          </Flex>
+        </OverflowWrapper>
+      </PageContainer>
+      <PetitionFooter locale={'HKChinese'} />
+      {!inView && (
+        <FixedCTA onClick={executeScroll}>
+          {formContent.mobile_cta ? formContent.mobile_cta : '立即捐款'}
+        </FixedCTA>
+      )}
+    </>
+  );
+}
+
+const mapStateToProps = ({ status, theme, signup }) => {
+  return { status, theme: theme.data, signup: signup.data };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setFormContent: (data) => {
+      dispatch({ type: formActions.SET_FORM, data });
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Index);
