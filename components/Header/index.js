33f811b3784e52c3e6bb4a71bf38b94f0befbf50
KFC <chris.chan@greenpeace.org>
2021-12-30 10:23:38 +0800

    update: Header component style

--- components/Header/index.js
+++ components/Header/index.js
@@ -6,17 +6,23 @@ import logoChinese from '@common/images/logo/GP-logo-2019-TC-white-[web]-01.png'
 
 const WithSubnavigation = ({ href }) => {
   return (
-    <Box bgColor={'brand.500'}>
-      <Flex py={{ base: 2 }} align={'center'}>
+    <Box
+      bgColor={'brand.500'}
+      borderBottom={'1px solid var(--shades-100)'}
+      boxShadow={'var(--shadow-1)'}
+    >
+      <Flex py={'12px'} align={'center'}>
         <Container maxW={'1200px'}>
-          <Link href={href ? href : null} isExternal="true">
-            <Image
-              src={logoChinese}
-              maxW="220px"
-              padding="2px"
-              alt={'Greenpeace 綠色和平'}
-            />
-          </Link>
+          <Box>
+            <Link href={href ? href : null} isExternal="true">
+              <Image
+                src={logoChinese}
+                maxW="220px"
+                padding="2px"
+                alt={'Greenpeace 綠色和平'}
+              />
+            </Link>
+          </Box>
         </Container>
       </Flex>
     </Box>
