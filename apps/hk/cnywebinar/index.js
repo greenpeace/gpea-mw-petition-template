736c34c4b3ee8c7be523c67a0ccd7d06de08da9b
KFC <chris.chan@greenpeace.org>
2022-01-04 17:55:43 +0800

    update: cny main page

--- apps/hk/cnywebinar/index.js
+++ apps/hk/cnywebinar/index.js
@@ -6,14 +6,7 @@ import FormContainer from '@containers/formContainer';
 import PetitionFooter from '@containers/petitionFooter';
 import { useInView } from 'react-intersection-observer';
 import { connect } from 'react-redux';
-import {
-  Avatar,
-  AvatarGroup,
-  Box,
-  Flex,
-  Container,
-  Text,
-} from '@chakra-ui/react';
+import { Avatar, AvatarGroup, Box, Flex, Text } from '@chakra-ui/react';
 import formContent from './form';
 import SEO from './SEO';
 import * as formActions from 'store/actions/action-types/form-actions';
@@ -56,30 +49,53 @@ function Index({ status, theme, setFormContent, signup }) {
   return (
     <>
       <SEO />
-      {submitted ? (
-        <ThanksBanner
-          bgImage={heroBannerImage}
-          content={{
-            title: `${
-              FirstName ? FirstName : '綠色和平支持者'
-            }，感謝您報名綠色新春教室`,
-            description: ['為無塑環境多走一步，捐助支持走塑項目。'],
-            colorCode: '#fcf5c5',
-          }}
-          removeMask="true"
-        />
-      ) : (
-        <HeroBanner
-          bgImage={heroBannerImage}
-          content={{
-            title:
-              '請即報名綠色新春教室<br/>裸買店店主同你<br/>環保辦年貨大掃除',
-            description: [''],
-            colorCode: '#fcf5c5',
-          }}
-          removeMask="true"
-        />
-      )}
+      <Box className="cny">
+        {submitted ? (
+          <ThanksBanner
+            bgImage={heroBannerImage}
+            content={{
+              title: `${
+                FirstName ? FirstName : '綠色和平支持者'
+              }，感謝您報名綠色新春教室`,
+              description: ['為無塑環境多走一步，捐助支持走塑項目。'],
+            }}
+            removeMask="true"
+          ></ThanksBanner>
+        ) : (
+          <HeroBanner
+            bgImage={heroBannerImage}
+            content={{
+              title:
+                '請即報名綠色新春教室<br/>裸買店店主同你<br/>環保辦年貨大掃除',
+              description: [''],
+            }}
+            removeMask="true"
+          >
+            <Flex pos="relative" zIndex="2" py={4} flexDirection="column">
+              <Text fontSize="xl" px="2" mb="4" fontWeight="bold" color="white">
+                嘉賓主持：
+              </Text>
+              <AvatarGroup size="xl" max={4}>
+                <Avatar
+                  name="「四圍斟」Susan"
+                  src={speaker3}
+                  onClick={() => scrollToRef(speaker3Ref)}
+                />
+                <Avatar
+                  name="譚穎琳 Leanne"
+                  src={speaker1}
+                  onClick={() => scrollToRef(speaker1Ref)}
+                />
+                <Avatar
+                  name="柯家文 Kaman"
+                  src={speaker2}
+                  onClick={() => scrollToRef(speaker2Ref)}
+                />
+              </AvatarGroup>
+            </Flex>
+          </HeroBanner>
+        )}
+      </Box>
       <PageContainer>
         <OverflowWrapper>
           <Flex flexDirection={{ base: 'column-reverse', md: 'row' }}>
@@ -97,37 +113,6 @@ function Index({ status, theme, setFormContent, signup }) {
               </ContentContainer>
             </Box>
             <Box flex={1} ref={myRef}>
-              {/**TODO: margin top issue */}
-              <Container>
-                <Flex pos="relative" zIndex="2" py={4} flexDirection="column">
-                  <Text
-                    fontSize="xl"
-                    px="2"
-                    mb="4"
-                    fontWeight="bold"
-                    color="white"
-                  >
-                    嘉賓主持：
-                  </Text>
-                  <AvatarGroup size="xl" max={4}>
-                    <Avatar
-                      name="「四圍斟」Susan"
-                      src={speaker3}
-                      onClick={() => scrollToRef(speaker3Ref)}
-                    />
-                    <Avatar
-                      name="譚穎琳 Leanne"
-                      src={speaker1}
-                      onClick={() => scrollToRef(speaker1Ref)}
-                    />
-                    <Avatar
-                      name="柯家文 Kaman"
-                      src={speaker2}
-                      onClick={() => scrollToRef(speaker2Ref)}
-                    />
-                  </AvatarGroup>
-                </Flex>
-              </Container>
               <FormContainer>
                 <Box ref={ref}>
                   {submitted ? <DonateForm /> : <SignupForm />}
