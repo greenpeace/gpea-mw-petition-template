6ae7e0a08449ba20123b0f739053f242c234b78f
KFC <chris.chan@greenpeace.org>
2022-01-04 10:54:50 +0800

    update: banner line height

--- components/Banner/thanks.js
+++ components/Banner/thanks.js
@@ -34,7 +34,7 @@ export default function Index({ content, bgImage }) {
                   <Heading
                     as="h1"
                     fontSize={{ base: 'var(--text-xl)', md: 'var(--text-2xl)' }}
-                    lineHeight="1.2"
+                    lineHeight="1.3"
                     color={'white'}
                     dangerouslySetInnerHTML={{ __html: content.title }}
                   />
