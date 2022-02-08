05138d07873e81e8fc9c2e3e6f9711b679e63eb8
KFC <chris.chan@greenpeace.org>
2022-02-08 17:17:51 +0800

    update: content

--- apps/hk/arctictest/resultContent/contentResult.js
+++ apps/hk/arctictest/resultContent/contentResult.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import {
-  Heading,
-  Box,
-  Text,
-  Image,
-  UnorderedList,
-  ListItem,
-} from '@chakra-ui/react';
+import { Heading, Box, Text, Image } from '@chakra-ui/react';
 import {
   headingProps,
   paragraphProps,
@@ -23,20 +16,6 @@ const Content = ({ theme }) => {
 
       <Text as="p" {...paragraphProps}>
         您的聯署將幫助綠色和平從個人、社區、企業與政策層面，推動源頭減少即棄塑膠，長遠解決塑膠污染問題，令美麗海洋與環境回復潔淨健康！
-      </Text>
-
-      <Heading {...headingProps} color={`theme.${themeInterests}`}>
-        拯救被塑膠殘害的生命，您可以施以援手！
-      </Heading>
-
-      <Text as="p" {...paragraphProps}>
-        即棄塑膠帶來的一時便利，換來充斥塑膠垃圾的海洋、環境永久的傷害 —
-        2019年，香港每日平均有約2,320公噸廢塑膠棄置堆填區；
-        全球每年約有1,270萬噸塑膠垃圾落入海洋，多達 52% 海龜誤食過塑膠...
-      </Text>
-
-      <Text as="p" {...paragraphProps}>
-        自2016年起，綠色和平致力推動源頭減塑，由推動快餐、超市等企業淘汰即棄塑膠、招募社區商戶成為走塑店鋪、督促政府訂立更積極的減塑目標與政策，竭力解決塑膠污染，幫助海洋、環境逐步回復潔淨健康，減輕下一代面對的環境負擔。
       </Text>
     </>
   );
---
+++ apps/hk/arctictest/resultContent/oceanResult.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Heading, Button, Box, Text, Image } from '@chakra-ui/react';
+import {
+  headingProps,
+  paragraphProps,
+} from '@common/styles/components/contentStyle';
+
+import image00 from '../images/GIY工作坊_R1.jpg';
+
+const Content = ({ theme }) => {
+  const themeInterests = theme.interests;
+  return (
+    <>
+      <Heading {...headingProps} color={`theme.${themeInterests}`}>
+        心理測驗結果會於1分鐘後傳送到您的電郵。
+      </Heading>
+
+      <Text as="p" {...paragraphProps}>
+        再一次感謝您的參與，找出代表您的海洋動物。但您知道在浩瀚的海洋中，這些動物正遇到種種困難，威脅著牠們的家園嗎？
+      </Text>
+
+      <Text as="p" {...paragraphProps}>
+        海洋孕育無數生物，亦提供地球50%的氧氣，更具調節全球氣候的功能，是我們的無上瑰寶。可惜，現時海洋受過度捕撈、深海採礦、塑膠污染等人類行為嚴重威脅，加上氣候危機影響下，海洋酸化、珊瑚白化問題愈趨嚴峻，海洋生態岌岌可危。
+      </Text>
+
+      <Text as="p" {...paragraphProps}>
+        如我們未能及時挽救這場危機，海洋生態系統、生物多樣性將受到嚴重破壞，珊瑚及與其共生的魚類可能就此滅絕。如若海洋陷入困境，地球環境亦將難逃厄運。
+      </Text>
+
+      <Text as="p" {...paragraphProps}>
+        綠色和平一直以行動、科學研究、政策倡議等方式守護海洋，您願意多走一步，捐助守護海洋的工作嗎？
+      </Text>
+
+      <Text as="p" {...paragraphProps}>
+        現以每月$200捐款支持守護海洋，您將獲得兩次「環保手作工作坊」或導賞團的機會！
+        名額有限，額滿即止！
+      </Text>
+
+      <Box {...paragraphProps}>
+        <Image src={image00} maxW="480px" mx="auto" />
+      </Box>
+
+      <Text as="p" {...paragraphProps}>
+        您的捐款將直接資助我們的海上科研任務、揭露企業破壞海洋的行為、以及國際間的倡議工作，直接幫助達成2030年30%全球海洋保護區的目標。
+      </Text>
+    </>
+  );
+};
+
+const mapStateToProps = ({ status, theme }) => {
+  return { status, theme: theme.data };
+};
+
+export default connect(mapStateToProps)(Content);
