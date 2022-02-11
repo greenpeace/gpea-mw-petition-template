3bb21970524fab2957e2bd796770e211f2d06ddb
KFC <chris.chan@greenpeace.org>
2022-02-11 15:23:51 +0800

    Update quiz.js

--- apps/hk/arctictest/quiz.js
+++ apps/hk/arctictest/quiz.js
@@ -26,17 +26,7 @@ const Quiz = ({ quiz, current }) => {
       <Container maxW={{ base: 'xl', xl: '2xl' }} pos={'relative'} zIndex={10}>
         <Center py={4}>
           <Stack w="100%" direction="column" spacing={4}>
-            {/* <Box bgColor={'#025177'} borderRadius={'8px'} p={4}>
-              <Heading
-                fontSize={{ base: 'md', md: 'xl' }}
-                color={'#FFF'}
-                textAlign={{ base: 'left', md: 'center' }}
-              >
-                找出隱藏在您潛意識中的極地動物！
-              </Heading>
-            </Box> */}
-            {current == 0 && (
-              <Box borderRadius={'8px'} p={4}>
+            {/* <Box borderRadius={'8px'} p={4}>
                 <Heading
                   fontSize={{ base: 'xl', md: '2xl' }}
                   color={'#025177'}
@@ -44,8 +34,7 @@ const Quiz = ({ quiz, current }) => {
                 >
                   找出隱藏在您潛意識中的極地動物！
                 </Heading>
-              </Box>
-            )}
+              </Box> */}
             <Box bgColor={'rgba(255,255,255,0.8)'} borderRadius={'8px'} p={4}>
               <Heading
                 fontSize={{ base: 'md', md: 'xl' }}
