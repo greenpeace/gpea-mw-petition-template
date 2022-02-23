32e18e1cff8969f470c8b0a5e963965760653e30
KFC <chris.chan@greenpeace.org>
2022-02-23 09:47:27 +0800

    Squashed commit of the following:
    commit 84707da51f7684f0de9b2a3ab165a1c66af20282
    Author: CK Lam <ckhk0621@gmail.com>
    Date:   Wed Feb 23 00:15:44 2022 +0800
    
        mask layer during the form submitting
    
    commit 4dd3f398604bdacfa11b9e4d71bbf30f8c78b0af
    Author: CK Lam <ckhk0621@gmail.com>
    Date:   Tue Feb 22 23:29:53 2022 +0800
    
        [WIP]Revamp layout
    
    commit 8012ef21449b7d352103d33b4fd47a82098bb82d
    Author: CK Lam <ckhk0621@gmail.com>
    Date:   Mon Feb 21 14:48:19 2022 +0800
    
        content update
    
    commit a6ec40c50a475afb6a66e274b8101596f97d4894
    Author: CK Lam <ckhk0621@gmail.com>
    Date:   Sun Feb 20 18:26:49 2022 +0800
    
        first init

---
+++ apps/hk/recyle101/Speaker.js
@@ -0,0 +1,101 @@
+import {
+  Avatar,
+  Flex,
+  SimpleGrid,
+  useColorModeValue,
+  Text,
+  Stack,
+} from '@chakra-ui/react';
+import { connect } from 'react-redux';
+import { paragraphProps } from '@common/styles/components/contentStyle';
+
+const pStyle = paragraphProps;
+
+// const backgrounds = [
+//   `url("data:image/svg+xml, %3Csvg xmlns=\'http://www.w3.org/2000/svg\' width=\'560\' height=\'185\' viewBox=\'0 0 560 185\' fill=\'none\'%3E%3Cellipse cx=\'102.633\' cy=\'61.0737\' rx=\'102.633\' ry=\'61.0737\' fill=\'%23ED64A6\' /%3E%3Cellipse cx=\'399.573\' cy=\'123.926\' rx=\'102.633\' ry=\'61.0737\' fill=\'%23F56565\' /%3E%3Cellipse cx=\'366.192\' cy=\'73.2292\' rx=\'193.808\' ry=\'73.2292\' fill=\'%2338B2AC\' /%3E%3Cellipse cx=\'222.705\' cy=\'110.585\' rx=\'193.808\' ry=\'73.2292\' fill=\'%23ED8936\' /%3E%3C/svg%3E")`,
+// ]
+
+function TestmonialCard(props) {
+  const { name, role, content, avatar, theme } = props;
+  const themeInterests = theme.interests;
+  return (
+    <Flex
+      boxShadow={'lg'}
+      direction={{ base: 'column', xl: 'row' }}
+      width={'full'}
+      rounded={'xl'}
+      px={6}
+      py={8}
+      justifyContent={'space-between'}
+      position="realtive"
+      border="1px"
+      borderColor="gray.100"
+      bg={useColorModeValue('white', 'gray.800')}
+      _before={{
+        content: '""',
+        position: 'absolute',
+        zIndex: '-1',
+        height: 'full',
+        width: 'full',
+        filter: 'blur(10px)',
+        transform: 'scale(0.98)',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        top: 0,
+        left: 0,
+        // backgroundImage: backgrounds[index % 4],
+      }}
+    >
+      <Avatar
+        src={avatar}
+        height={'120px'}
+        width={'120px'}
+        alignSelf={'center'}
+        m={{ base: '0 0 20px 0', xl: '0 20px 0 0' }}
+      />
+      <Stack
+        direction={'column'}
+        textAlign={'left'}
+        justifyContent={'space-between'}
+        spacing="4"
+      >
+        <Text fontWeight="bold">
+          {name}
+          <br />
+          <Text as="span" fontSize="sm" color={'gray.500'}>
+            {role}
+          </Text>
+        </Text>
+        <Text {...pStyle}>{content}</Text>
+      </Stack>
+    </Flex>
+  );
+}
+
+function GridBlurredBackdrop({ content, theme }) {
+  return (
+    <Flex
+      textAlign={'center'}
+      justifyContent={'center'}
+      direction={'column'}
+      width={'full'}
+    >
+      <SimpleGrid columns={{ base: 1 }} spacing={'20'} mb={6} mx={'auto'}>
+        {content.map((cardInfo, index) => (
+          <TestmonialCard
+            {...cardInfo}
+            index={index}
+            theme={theme}
+            key={index}
+          />
+        ))}
+      </SimpleGrid>
+    </Flex>
+  );
+}
+
+const mapStateToProps = ({ theme }) => {
+  return { theme: theme.data };
+};
+
+export default connect(mapStateToProps)(GridBlurredBackdrop);
---
+++ apps/hk/recyle101/index.js
@@ -0,0 +1,90 @@
+import React, { useEffect, useState, useRef } from 'react';
+import dynamic from 'next/dynamic';
+import PetitionFooter from '@containers/petitionFooter';
+import { useInView } from 'react-intersection-observer';
+import { connect } from 'react-redux';
+import { Box, Container, useMediaQuery } from '@chakra-ui/react';
+import HeroSection from './components/HeroSection';
+import MainSection from './components/MainSection';
+import Form from './components/Form';
+import formContent from './form';
+import SEO from './SEO';
+import * as formActions from 'store/actions/action-types/form-actions';
+
+import heroBannerImage from './images/q1-cny-webinar-kv-banner.jpg';
+
+const FixedCTA = dynamic(() => import('@components/GP/FixedCTA'));
+
+const maxWSize = 1200;
+
+function Index({ setFormContent }) {
+  const [isLargerThanMD] = useMediaQuery('(min-width: 48em)'); // default md: '48em'
+  const { ref, inView } = useInView({ threshold: 0 });
+  const mobileForm = useRef(null);
+  const executeScroll = (ref) => {
+    ref.current?.scrollIntoView({ behavior: 'smooth' });
+  };
+  const [showCTAButton, setShowCTAButton] = useState(false);
+
+  useEffect(() => {
+    setFormContent(formContent);
+  }, []);
+
+  useEffect(() => {
+    if (isLargerThanMD) {
+      setShowCTAButton(false);
+      return;
+    }
+    if (!inView && !isLargerThanMD) {
+      setShowCTAButton(true);
+    } else {
+      setShowCTAButton(false);
+    }
+  }, [inView, isLargerThanMD]);
+
+  return (
+    <>
+      <SEO />
+      <Box bgImage={heroBannerImage} bgRepeat={'no-repeat'} bgSize={'cover'}>
+        <Container maxW={`${maxWSize}px`}>
+          <HeroSection />
+        </Container>
+      </Box>
+
+      {/** Mobile form */}
+      <Box ref={mobileForm}>
+        <Box d={{ base: 'block', md: 'none' }} mt={-2} ref={ref}>
+          <Form />
+        </Box>
+      </Box>
+      {/** Mobile form End */}
+
+      <Container maxW={`${maxWSize}px`}>
+        <Box w={{ md: 'md', xl: 'xl' }} py={10}>
+          <MainSection />
+        </Box>
+      </Container>
+
+      <PetitionFooter locale={'HKChinese'} />
+      {showCTAButton && (
+        <FixedCTA onClick={() => executeScroll(mobileForm)}>
+          {formContent.mobile_cta ? formContent.mobile_cta : '立即捐款'}
+        </FixedCTA>
+      )}
+    </>
+  );
+}
+
+const mapStateToProps = ({ status, theme, signup }) => {
+  return { status, theme: theme.data, signup: signup.data };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setFormContent: (data) => {
+      dispatch({ type: formActions.SET_FORM, data });
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Index);
