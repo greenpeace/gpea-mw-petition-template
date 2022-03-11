b40891fc6ddda24f9f62658b63eb5e4378aa6cac
KFC <chris.chan@greenpeace.org>
2022-03-11 14:47:56 +0800

    update styles

--- components/GPS/CTA/button.js
+++ components/GPS/CTA/button.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Button, Text } from '@chakra-ui/react';
 
 export const CTAButton = () => (
   <Box
-    bgColor={'green.500'}
+    bgColor={'orange.300'}
     borderRadius={8}
     _hover={{ opacity: 0.9 }}
     cursor={'pointer'}
     align="center"
     p={1}
   >
-    <Box bgColor={'green.700'} borderRadius={8} p={4} align="center">
+    <Box bgColor={'orange.500'} borderRadius={8} p={4} align="center">
       <Text fontSize={{ base: 'lg', md: 'xl' }} color={'#FFF'} fontWeight={700}>
         立即免費使用走塑GPS/立即啟動走塑GPS
       </Text>
