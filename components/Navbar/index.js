a62562de562a37378ece0283add7344b3125e39c
KFC <chris.chan@greenpeace.org>
2022-03-15 16:01:28 +0800

    revamp navbar

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -26,63 +26,69 @@ export default function WithSubnavigation() {
       zIndex="9"
       pos={'fixed'}
       top={0}
-      w={'100%'}
       h={{ base: '50px', md: '60px' }}
-      bgColor={'#FFF'}
+      w="100%"
+      bgColor="white"
       py={1}
       ref={ref}
       borderBottom="1px"
       borderColor="gray.100"
     >
-      <Flex px={{ base: 4 }} align={'center'} justifyContent={'space-around'}>
-        <Flex
-          flex={{ base: 1, md: 'auto' }}
-          ml={{ base: -2 }}
-          display={{ base: 'flex', md: 'none' }}
-        >
-          <IconButton
-            onClick={onToggle}
-            icon={
-              isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />
-            }
-            variant={'ghost'}
-            aria-label={'Toggle Navigation'}
+      <Box maxW="1200px" mx="auto">
+        <Flex px={{ base: 4 }} align={'center'} justifyContent={'space-around'}>
+          <Flex
+            flex={{ base: 1, md: 'auto' }}
+            ml={{ base: -2 }}
+            display={{ base: 'flex', md: 'none' }}
+          >
+            <IconButton
+              onClick={onToggle}
+              icon={
+                isOpen ? (
+                  <CloseIcon w={3} h={3} />
+                ) : (
+                  <HamburgerIcon w={5} h={5} />
+                )
+              }
+              variant={'ghost'}
+              aria-label={'Toggle Navigation'}
+              sx={{
+                ':focus:not(:focus-visible)': {
+                  shadow: 'none',
+                },
+              }}
+            />
+          </Flex>
+          <Link
+            href={'/'}
             sx={{
               ':focus:not(:focus-visible)': {
                 shadow: 'none',
               },
             }}
-          />
+            cursor={'pointer'}
+          >
+            <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}>
+              <Image
+                loader={imageLoader}
+                src={`/images/greenpeace_logo.svg`}
+                layout="intrinsic"
+                width={'160px'}
+                height={'30px'}
+              />
+            </Flex>
+          </Link>
+
+          <Box d={{ base: 'none', md: 'block' }}>
+            <DesktopNav />
+          </Box>
+          <Box d={{ base: 'block', md: 'none' }} flex={1} />
         </Flex>
-        <Link
-          href={'/'}
-          sx={{
-            ':focus:not(:focus-visible)': {
-              shadow: 'none',
-            },
-          }}
-          cursor={'pointer'}
-        >
-          <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}>
-            <Image
-              loader={imageLoader}
-              src={`/images/greenpeace_logo.svg`}
-              layout="intrinsic"
-              width={'160px'}
-              height={'30px'}
-            />
-          </Flex>
-        </Link>
 
-        <Box d={{ base: 'none', md: 'block' }}>
-          <DesktopNav />
-        </Box>
-        <Box d={{ base: 'block', md: 'none' }} flex={1} />
-      </Flex>
-
-      <Collapse in={isOpen} animateOpacity>
-        <MobileNav handleToggle={onToggle} />
-      </Collapse>
+        <Collapse in={isOpen} animateOpacity>
+          <MobileNav handleToggle={onToggle} />
+        </Collapse>
+      </Box>
     </Box>
   );
 }
@@ -95,43 +101,45 @@ const DesktopNav = () => {
     <Flex zIndex="1" d={{ base: 'flex' }} h={'50px'} align={'center'}>
       {NAV_ITEMS.map((navItem) => (
         <Box key={navItem.label} px={2}>
-          {navItem.target === '_blank' ? (
-            <a href={navItem.href} target={navItem.target}>
-              <Text
-                bgColor="orange.500"
-                fontSize={'md'}
-                fontWeight={700}
-                px={4}
-                py={2}
-                color={'white'}
-                borderRadius={'4px'}
-              >
-                {navItem.label}
-              </Text>
-            </a>
-          ) : (
-            <Link href={navItem.href ?? '#'}>
-              <Text
-                p={2}
-                fontSize={'md'}
-                color={linkColor}
-                _hover={{
-                  textDecoration: 'none',
-                  color: linkHoverColor,
-                }}
-                sx={{
-                  ':focus:not(:focus-visible)': {
-                    shadow: 'none',
-                  },
-                }}
-                cursor={'pointer'}
-              >
-                {navItem.label}
-              </Text>
-            </Link>
-          )}
+          <Link href={navItem.href ?? '#'}>
+            <Text
+              p={2}
+              fontSize={'md'}
+              color={linkColor}
+              _hover={{
+                textDecoration: 'none',
+                color: linkHoverColor,
+              }}
+              sx={{
+                ':focus:not(:focus-visible)': {
+                  shadow: 'none',
+                },
+              }}
+              cursor={'pointer'}
+            >
+              {navItem.label}
+            </Text>
+          </Link>
         </Box>
       ))}
+      <Box d="inline-block">
+        <Link
+          href="https://supporter.ea.greenpeace.org/hk/s/donate/alt-layout?language=zh_HK&campaign=plastics_mw&ref=gps-header-donate-button"
+          isExternal
+        >
+          <Text
+            bgColor="orange.500"
+            fontSize={'md'}
+            fontWeight={700}
+            px={4}
+            py={2}
+            color={'white'}
+            borderRadius={'4px'}
+          >
+            捐助支持
+          </Text>
+        </Link>
+      </Box>
     </Flex>
   );
 };
@@ -145,6 +153,7 @@ const MobileNav = ({ handleToggle }) => {
       px="6"
       pt="6"
       pb="10"
+      w="100vw"
       display={{ md: 'none' }}
       borderBottom="1px"
       borderColor="gray.100"
@@ -156,6 +165,24 @@ const MobileNav = ({ handleToggle }) => {
           {...navItem}
         />
       ))}
+      <Box d="inline-block">
+        <Link
+          href="https://supporter.ea.greenpeace.org/hk/s/donate/alt-layout?language=zh_HK&campaign=plastics_mw&ref=gps-header-donate-button"
+          isExternal
+        >
+          <Text
+            bgColor="orange.500"
+            fontSize={'md'}
+            fontWeight={700}
+            px={4}
+            py={2}
+            color={'white'}
+            borderRadius={'4px'}
+          >
+            捐助支持
+          </Text>
+        </Link>
+      </Box>
     </Stack>
   );
 };
@@ -205,10 +232,5 @@ const NAV_ITEMS = [
     label: '常見問題',
     href: '/faq',
     target: '_self',
-  },
-  {
-    label: '捐助支持',
-    href: 'https://supporter.ea.greenpeace.org/hk/s/donate/alt-layout?language=zh_HK&campaign=plastics_mw&ref=gps-header-donate-button',
-    target: '_blank',
   },
 ];
