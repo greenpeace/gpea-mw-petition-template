5262ec8b5f9415060cb8848faa40a6be91dbae47
CK Lam <ckhk0621@gmail.com>
2022-03-15 22:51:53 +0800

    whatsapp button in mobile

--- containers/gpsWrapper.js
+++ containers/gpsWrapper.js
@@ -1,16 +1,48 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { connect, dispatch } from 'react-redux';
 import { Box } from '@chakra-ui/react';
 import Nav from 'components/Navbar';
 import PetitionFooter from 'containers/petitionFooter';
 import * as themeActions from 'store/actions/action-types/theme-actions';
+import { imageLoader } from 'common/utils';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 const envProjectName = process.env.projectName;
 const envProjectMarket = process.env.projectMarket;
 const themeEndpointURL = process.env.themeEndpoint;
 
 function Layout({ children, themeData, setTheme }) {
+  const router = useRouter();
+  const [showChat, setShowChat] = useState(false);
+  useEffect(async () => {
+    const singleResult = await axios
+      .get(themeEndpointURL)
+      .then((response) => {
+        return response.data.records.find(
+          (d) =>
+            d.ProjectName === envProjectName && d.Market === envProjectMarket,
+        );
+      })
+      .catch((error) => console.log(error));
+
+    console.log('Building from ' + envProjectMarket + ':' + envProjectName);
+
+    !singleResult && console.warn('PROJECT NAME NOT FOUND');
+
+    setTheme(singleResult);
+  }, []);
+
+  useEffect(async () => {
+    const regex = /registration/;
+    if (router.route?.match(regex) !== null) {
+      setShowChat(false);
+      return;
+    }
+    setShowChat(true);
+  }, [router]);
+
   useEffect(async () => {
     const singleResult = await axios
       .get(themeEndpointURL)
@@ -34,6 +66,19 @@ function Layout({ children, themeData, setTheme }) {
       <Nav />
       {children}
       <PetitionFooter locale={'HKChinese'} />
+      <Box pos={'fixed'} bottom={'20px'} right={'20px'} d={{ md: 'none' }}>
+        <a
+          href="https://wa.me/15551234567/?text=走塑GPS 全港走塑店鋪定位地圖"
+          target="_blank"
+        >
+          <Image
+            loader={imageLoader}
+            src={`/images/whatsapp.svg`}
+            width="48px"
+            height="48px"
+          />
+        </a>
+      </Box>
     </Box>
   );
 }
