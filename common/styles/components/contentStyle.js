da982fbe1cf36975a6e6c0c8972d77adc6b20ee6
KFC <chris.chan@greenpeace.org>
2022-03-30 23:42:08 +0800

    Revert "Update contentStyle.js"
    This reverts commit 255ba18dd57f9d31c1f6ba0405c5b86cc044ef4a.

--- common/styles/components/contentStyle.js
+++ common/styles/components/contentStyle.js
@@ -1,13 +1,12 @@
 export const headingProps = {
-  color: 'gray.900',
   lineHeight: 1.2,
   mb: 6,
   fontSize: { base: 'xl', md: '2xl' },
 };
 export const paragraphProps = {
-  color: 'gray.700',
   lineHeight: 2,
   mb: 6,
+  fontSize: { base: '16px' },
   whiteSpace: 'pre-line',
   textAlign: 'justify',
 };
