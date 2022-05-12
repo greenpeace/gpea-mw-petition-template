8d1eeb9a1ba9b0d62e497d79a6aa935890a28cc5
KFC <53209063+eahk@users.noreply.github.com>
2022-05-12 12:54:24 +0800

    update: logo opacity

--- apps/hk/lantau-documentary/components/HeroSection/logo.js
+++ apps/hk/lantau-documentary/components/HeroSection/logo.js
@@ -1,37 +1,46 @@
 import React from 'react';
 import {
-    useAnimation,
-    useViewportScroll,
-    motion,
-    useTransform,
-  } from 'framer-motion';
+  useAnimation,
+  useViewportScroll,
+  motion,
+  useTransform,
+} from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
 const AnimationBox = ({ children }) => {
-    const { scrollY, scrollYProgress } = useViewportScroll();
+  const { scrollY, scrollYProgress } = useViewportScroll();
 
-    const y = useTransform(scrollY, [0, 400], [0, -200]);
-    const opacity = useTransform(scrollYProgress, (value) => 1 - value * 30);
-    const scale = useTransform(scrollYProgress, (value) => 1 - value * 10);
-    // const control = useAnimation();
-    const [ref, inView] = useInView({
-      /* Optional options */
-      threshold: 0.5,
-      triggerOnce: false
-    });
+  const y = useTransform(scrollY, [0, 400], [0, -200]);
+  const opacity = useTransform(scrollYProgress, (value) => 1 - value * 20);
+  const scale = useTransform(scrollYProgress, (value) => 1 - value * 10);
+  // const control = useAnimation();
+  const [ref, inView] = useInView({
+    /* Optional options */
+    threshold: 0.5,
+    triggerOnce: false,
+  });
 
-    console.log('trigger')
+  console.log('trigger');
 
-    return (
-      <div>
-        <div className="absolute top-[100px] md:top-[180px]" ref={ref}></div> {/** InView point */}
-        {inView && <motion.div
+  return (
+    <div>
+      <div className="absolute top-[100px] md:top-[180px]" ref={ref}></div>{' '}
+      {/** InView point */}
+      {/* {inView && (
+        <motion.div
           className="box"
-          style={{ y: y, opacity: opacity, scale: scale}}
+          style={{ y: y, opacity: opacity, scale: scale }}
         >
           {children}
-        </motion.div>}
-      </div>
-    );
-  };
+        </motion.div>
+      )} */}
+      <motion.div
+        className="box"
+        style={{ y: y, opacity: opacity, scale: scale }}
+      >
+        {children}
+      </motion.div>
+    </div>
+  );
+};
 
 export default AnimationBox;
