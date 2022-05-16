9a0642b9b9f9d856fa9380c21756fe0577bac773
KFC <53209063+eahk@users.noreply.github.com>
2022-05-16 15:15:39 +0800

    Squashed commit of the following:
    commit 78ebbaf017976ea132fd530065e1398a2a1346e6
    Author: KFC <53209063+eahk@users.noreply.github.com>
    Date:   Mon May 16 15:15:22 2022 +0800
    
        update banner
    
    commit c5ea993c2498de059c5f9ebb46f657b6e50fa791
    Author: KFC <53209063+eahk@users.noreply.github.com>
    Date:   Mon May 16 14:47:20 2022 +0800
    
        update
    
    commit b335b78e0980556426eab2833541ab163b24aa87
    Author: KFC <53209063+eahk@users.noreply.github.com>
    Date:   Mon May 16 13:38:54 2022 +0800
    
        fix missing scroll function
    
    commit d61bcb775cb4abbeb1df50bde61266c230e50472
    Author: KFC <53209063+eahk@users.noreply.github.com>
    Date:   Mon May 16 13:25:55 2022 +0800
    
        update
    
    commit b38e399d2563af4c15613b023413827c236fb595
    Author: bijen.cjen <bijen.chen@greenpeace.org>
    Date:   Fri May 13 18:51:30 2022 +0800
    
        update: plasticfree marketplace (recover main banner)
    
    commit 2cdc43ce02d752c6be225416d49f0d9843f2361a
    Author: bijen.cjen <bijen.chen@greenpeace.org>
    Date:   Fri May 13 18:38:25 2022 +0800
    
        update: plasticfree marketplace
    
        1. update main banner image
        2. update thank you page content
        3. update form content
    
    commit 797dfa0927a517bd155ca48e709f2f6018fa2a3d
    Merge: 2c37b2d7 3deef788
    Author: bijen.cjen <bijen.chen@greenpeace.org>
    Date:   Tue May 10 12:28:56 2022 +0800
    
        Merge branch 'plasticfree-marketplace' of https://github.com/greenpeace/gpea-mw-petition-template.git; branch 'plasticfree-marketplace' of https://github.com/greenpeace/gpea-mw-petition-template into plasticfree-marketplace
    
    commit 2c37b2d79fbf46983d37685ce0eb0415e70930c8
    Author: bijen.cjen <bijen.chen@greenpeace.org>
    Date:   Mon May 9 18:23:15 2022 +0800
    
        update: plastic-free-marketplace
    
        1. special setting to top padding for form again
            - reason: for enough space
    
    commit 3deef78864251524eec9c40409de71f10824b107
    Author: bijen.cjen <bijen.chen@greenpeace.org>
    Date:   Mon May 9 18:23:15 2022 +0800
    
        create: plastic-free-marketplace
    
    commit 2f936f2fda106cc7d007db73453ef6572b04b814
    Merge: 901a6aca 6ba4b390
    Author: bijen.cjen <bijen.chen@greenpeace.org>
    Date:   Fri May 6 18:53:51 2022 +0800
    
        Merge branch 'develop' into plasticfree-marketplace
    
    commit 901a6aca5130e159c541b879b15b2fb2bd4bae38
    Merge: dca632f9 c7f11cb5
    Author: bijen.cjen <bijen.chen@greenpeace.org>
    Date:   Fri May 6 11:28:30 2022 +0800
    
        Merge branch 'develop' into plasticfree-marketplace

---
+++ apps/hk/plasticfree-marketplace/SEO.js
@@ -0,0 +1,45 @@
+import Head from 'next/head';
+import Script from 'next/script';
+
+const SEO = () => {
+  return (
+    <Head>
+      {/* campaign dataLayer */}
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          var dataLayer = (window.dataLayer = window.dataLayer || []);
+          dataLayer.push({
+            gCampaign: 'climate',
+            gBasket: 'earthdaywebinar',
+          });
+        `,
+        }}
+      />
+      {/* title */}
+      <title>
+        立即登記參與「惜簡生活節」，體驗裸買市集 - Greenpeace 綠色和平 |
+        香港
+      </title>
+      <meta
+        property="og:title"
+        content="立即登記參與「惜簡生活節」，體驗裸買市集 - Greenpeace 綠色和平 | 香港"
+      />
+      <meta
+        name="description"
+        content="今年6月，適逢世界環境日和國際海洋日，綠色和平舉辦「惜簡生活節」，帶領更多市民在日常中實踐裸買生活。現只需網上登記，即能參與「惜簡生活節」以及報名環保好生活工作坊。"
+      />
+      <meta
+        property="og:description"
+        content="今年6月，適逢世界環境日和國際海洋日，綠色和平舉辦「惜簡生活節」，帶領更多市民在日常中實踐裸買生活。現只需網上登記，即能參與「惜簡生活節」以及報名環保好生活工作坊。"
+      />
+      <meta
+        property="og:image"
+        content="https://www.greenpeace.org/static/planet4-hongkong-stateless/2022/05/a54fabd4-202205-plastic-free-marketplace-kv-fb-preview.png"
+      />
+    </Head>
+  );
+};
+
+export default SEO;
