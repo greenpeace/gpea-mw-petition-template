4509154f4015c57ff7d2701350211934cf985266
bijen.cjen <bijen.chen@greenpeace.org>
2022-06-07 18:57:52 +0800

    update: downloadable content

--- apps/hk/downloadable/SEO.js
+++ apps/hk/downloadable/SEO.js
@@ -19,24 +19,24 @@ const SEO = () => {
       />
       {/* title */}
       <title>
-        立即下載：綠色生活電子手冊新春篇 精華內容教你環保過年 - Greenpeace
+        立即下載：綠色生活電子手冊 精華內容教你環保好生活 - Greenpeace
         綠色和平 | 香港
       </title>
       <meta
         property="og:title"
-        content="立即下載：綠色生活電子手冊新春篇 精華內容教你環保過年 - Greenpeace 綠色和平 | 香港"
+        content="立即下載：綠色生活電子手冊 精華內容教你環保好生活 - Greenpeace 綠色和平 | 香港"
       />
       <meta
         name="description"
-        content="綠色和平推出綠色生活電子手冊新春篇，20頁精華內容包括團年飯素食食譜、大掃除簡約收納法、環保清潔劑做法等，教你方便又環保地過年！電子手冊免費下載，即下載了解生活減廢、重用資源貼士，培養新一年新習慣，為地球環境出一分力！"
+        content="綠色和平推出綠色生活電子手冊，20頁精華內容包括家常素食食譜、家居掃除簡約收納法、環保清潔劑做法等，教你方便又環保地生活！電子手冊免費下載，即下載了解生活減廢、重用資源貼士，培養環保新習慣，為地球環境出一分力！"
       />
       <meta
         property="og:description"
-        content="綠色和平推出綠色生活電子手冊新春篇，20頁精華內容包括團年飯素食食譜、大掃除簡約收納法、環保清潔劑做法等，教你方便又環保地過年！電子手冊免費下載，即下載了解生活減廢、重用資源貼士，培養新一年新習慣，為地球環境出一分力！"
+        content="綠色和平推出綠色生活電子手冊，20頁精華內容包括家常素食食譜、家居掃除簡約收納法、環保清潔劑做法等，教你方便又環保地生活！電子手冊免費下載，即下載了解生活減廢、重用資源貼士，培養環保新習慣，為地球環境出一分力！"
       />
       <meta
         property="og:image"
-        content="https://www.greenpeace.org/static/planet4-hongkong-stateless/2022/01/61150b1d-cny-booklet-preview-updatedv2.jpg"
+        content="https://www.greenpeace.org/static/planet4-hongkong-stateless/2022/06/7db0a0b3-202206-cny-ebook-new-fb-preview.jpg"
       />
     </Head>
   );
