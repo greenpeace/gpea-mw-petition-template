6b97a14de697be05e7f29d661fc9feaca8c610af
Chris Chan <chris.chan@greenpeace.org>
2022-07-05 13:51:50 +0800

    Update hk general hero section

--- apps/hk/general/components/HeroSection/Content.js
+++ apps/hk/general/components/HeroSection/Content.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { Box, Heading, Stack } from '@chakra-ui/react';
 import { useInView } from 'react-intersection-observer';
-import Form from '../Form';
+import DonationModule from '@components/GP/DonationModule';
 import { useWindowSize } from '@common/utils/index';
 import { headingProps } from '@common/styles/components/contentStyle';
 const formWidth = 500;
@@ -46,16 +46,30 @@ function Content({ children }) {
           textShadow="0 0 1px rgba(0,0,0, .2)"
           mb={4}
           dangerouslySetInnerHTML={{
-            __html: '請即下載<br/>山海大嶼電子書',
+            __html: '請即捐款<br/>拯救脆弱的地球生態！',
           }}
         />
       </Box>
       <Box pos={'relative'}>
-        {readyToShow && (
-          <Box {...formProps} d={{ base: 'none', lg: 'block' }}>
-            <Form />
+        <Box {...formProps} d={{ base: 'none', lg: 'block' }}>
+          <Box
+            mx="auto"
+            bgColor="white"
+            borderRadius={8}
+            boxShadow="lg"
+            overflow="hidden"
+            pos="relative"
+            zIndex={10}
+          >
+            <DonationModule
+              market={'HK'}
+              language={'zh_HK'}
+              campaign={'general'}
+              // campaignId={''}
+              env={'production'}
+            />
           </Box>
-        )}
+        </Box>
       </Box>
     </Stack>
   );
