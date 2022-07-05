ecbc88b1d185a73dcd26c410ba46dc1657826986
Chris Chan <chris.chan@greenpeace.org>
2022-07-05 16:59:17 +0800

    Align project form component index settings

--- apps/hk/recyclebooklet/components/Form/index.js
+++ apps/hk/recyclebooklet/components/Form/index.js
@@ -1,27 +1,26 @@
 import React from 'react';
-import dynamic from 'next/dynamic';
 import { connect } from 'react-redux';
-import { Container, Box } from '@chakra-ui/react';
-const DonateForm = dynamic(() => import('@components/GP/DonateForm'));
+import { Box } from '@chakra-ui/react';
+import DonateForm from '@components/GP/DonateForm';
+import SignupForm from '@components/GP/WebinarForm';
+import DonationModule from '@components/GP/DonationModule';
 const SignupForm = dynamic(() => import('@components/GP/WebinarForm'));
 
 const Index = ({ status }) => {
   const RenderForm = () =>
     status?.submitted ? <DonateForm /> : <SignupForm />;
   return (
-    <Container maxW={'100%'} padding="2px">
-      <Box
-        mx="auto"
-        bgColor="white"
-        border="1px"
-        borderColor="rgba(255,255,255,0.75)"
-        borderRadius={8}
-        boxShadow="lg"
-        overflow="hidden"
-      >
-        <RenderForm />
-      </Box>
-    </Container>
+    <Box
+      mx="auto"
+      bgColor="white"
+      borderRadius={8}
+      boxShadow="lg"
+      overflow="hidden"
+      pos="relative"
+      zIndex={10}
+    >
+      <RenderForm />
+    </Box>
   );
 };
 
