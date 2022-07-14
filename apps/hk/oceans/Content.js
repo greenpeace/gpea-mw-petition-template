1e95051d855e6c74c3cb85d9ca82ed348de5ebce
CK <ckhk0621@gmail.com>
2022-07-14 10:36:53 +0800

    revert content

--- apps/hk/oceans/Content.js
+++ apps/hk/oceans/Content.js
@@ -1,48 +1,66 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Heading, Text } from '@chakra-ui/react';
-import DonateFAQ from '@components/DonateFAQ';
+import {
+  Box,
+  Heading,
+  Image,
+  Text,
+} from '@chakra-ui/react';
 import {
   headingProps,
   paragraphProps,
 } from '@common/styles/components/contentStyle';
 
+import Image01 from './images/GP0STU61Y_PressMedia.jpg';
+import Image02 from './images/GP01B4T_PressMedia.jpg';
+
 const Content = ({ theme }) => {
   const themeInterests = theme.interests;
   return (
     <>
-      <Heading {...headingProps} color={`theme.${themeInterests}`}>
-      您願意進一步守護珍貴海洋嗎？立即捐款，攜手守護珍貴海洋。
+       <Heading {...headingProps} color={`theme.${themeInterests}`}>
+        聯署守護海洋
       </Heading>
 
       <Text as="p" {...paragraphProps}>
-      您的捐款將直接資助我們與全球頂尖科學家，進行海上科研任務、揭露企業破壞海洋的行為、以及國際間的倡議工作，直接幫助達成 2030年 30% 全球海洋保護區的目標。
+        您的聯署將幫助推動《全球海洋公約》，於2030年前將全球至少30%海洋納入保護區！
+      </Text>
+
+      <Text as="p" {...paragraphProps}>
+        綠色和平正推動各國政府支持《全球海洋公約》，為海洋建立完善保護機制，以長久保護、維持、復育海洋健康，並在2030年前成立至少30%的海洋保護區。
       </Text>
 
       <Heading {...headingProps} color={`theme.${themeInterests}`}>
-      海洋正向我們發出求救信號，您可以施以援手！
+        您願意聯署加入守護海洋行列嗎？
+      </Heading>
+
+      <Box {...paragraphProps}>
+        <Image src={Image01} layout="fill" alt="Greenpeace 綠色和平" />
+      </Box>
+
+      <Heading {...headingProps} color={`theme.${themeInterests}`}>
+        海洋正向我們發出求救信號，我們需要施予援手！
       </Heading>
 
       <Text as="p" {...paragraphProps}>
-      海洋現正面臨多方面的威脅，包括過度捕魚、深海採礦、塑膠及鑽油污染等，不少生物面臨滅絕危機，生物多樣性瀕臨崩潰邊緣。
+        海洋孕育無數生物，亦提供地球50%的氧氣，更具調節全球氣候的功能，是我們的無上瑰寶。可惜，現時海洋受過度捕撈、深海採礦、塑膠污染等人類行為嚴重威脅，加上氣候危機影響下，海洋酸化、珊瑚白化問題愈趨嚴峻，海洋生態岌岌可危。
       </Text>
 
       <Text as="p" {...paragraphProps}>
-      有您的捐助支持，綠色和平便能有更多資源與力量執行保護海洋工作，幫助海洋逐步回復美麗健康，守護海洋生態。
+        如我們未能及時挽救這場危機，海洋生態系統、生物多樣性將受到嚴重破壞，珊瑚及與其共生的魚類可能就此滅絕。如若海洋陷入困境，地球環境亦將難逃厄運。
       </Text>
 
-      <Heading {...headingProps}>向美麗大海伸出援手，捐助綠色和平海洋工作。</Heading>
+      <Box {...paragraphProps}>
+        <Image src={Image02} layout="fill" alt="Greenpeace 綠色和平" />
+      </Box>
 
-      <Heading
-        textAlign="center"
-        py="6"
-        {...headingProps}
-        color={`theme.${themeInterests}`}
-      >
-        常見問題
+      <Text as="p" {...paragraphProps}>
+        綠色和平正號召全球支持者加入海洋聯署，亦誠意邀請您參與聯署，壯大守護海洋力量。您的力量，是推動2030年前成立至少30%海洋保護區的關鍵。
+      </Text>
+
+      <Heading {...headingProps} color={`theme.${themeInterests}`}>
+        請即聯署，向正受破壞的美麗大海伸出援手，支持訂立《全球海洋公約》！
       </Heading>
-
-      <DonateFAQ locale="HKChinese" />
     </>
   );
 };
