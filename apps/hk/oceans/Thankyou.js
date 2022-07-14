59d1e1c3cf0ac540a878da03122b866c8c5b7680
CK <ckhk0621@gmail.com>
2022-07-14 10:36:53 +0800

    revert content

--- apps/hk/oceans/Thankyou.js
+++ apps/hk/oceans/Thankyou.js
@@ -4,7 +4,6 @@ import {
   headingProps,
   paragraphProps,
 } from '@common/styles/components/contentStyle';
-import DonateFAQ from '@components/DonateFAQ';
 import { useSelector } from 'react-redux';
 
 const Thankyou = () => {
@@ -15,41 +14,37 @@ const Thankyou = () => {
   return (
     <>
       <Heading {...headingProps}>
-      您願意進一步拯救脆弱的地球生態嗎？
+        感謝您聯署支持成立全球海洋保護區與訂立《全球海洋公約》。
       </Heading>
 
       <Text as="p" {...paragraphProps}>
-      您的捐款將直接資助我們揭露破壞環境真相、進行嚴謹的科學研究、直接行動，積極遊說政府、企業，凝聚社區力量，倡議對環境有利的政策和解決方案。
+        您的聯署意義重大，我們將一起實現保護、維持、復育海洋健康的理想。
       </Text>
 
+      <Text as="p" {...paragraphProps}>
+        綠色和平團隊一直以行動、科學研究、政策倡議等方式守護海洋，您願意多走一步，捐助支持我們將守護工作做得更廣泛有效嗎？
+      </Text>
+
+      <Text as="p" {...paragraphProps}>
+        您的捐款將直接資助我們的海上科研任務、揭露企業破壞海洋的行為、以及國際間的倡議工作，直接幫助達成
+        2030年30%全球海洋保護區的目標。
+      </Text>
 
       <Heading {...headingProps}>
-      地球的承受能力快將超出負荷，您可以施以援手！
+        海洋正向我們發出求救信號，您可以施以援手！
       </Heading>
 
       <Text as="p" {...paragraphProps}>
-      氣候危機來勢洶洶、北極海冰急速消融、森林大火屢見不鮮、海洋資源被無限索取，全球生物多樣性危在旦夕...
+        海洋現正面臨多方面的威脅，包括過度捕魚、深海採礦、塑膠及鑽油污染等，不少生物面臨滅絕危機，生物多樣性瀕臨崩潰邊緣。
       </Text>
 
       <Text as="p" {...paragraphProps}>
-      有您的捐助支持，綠色和平便能有更多資源與力量執行守護環境的工作，讓地球欣欣向榮，生生不息，為下一代創造美好未來。
+        有您的捐助支持，綠色和平便能有更多資源與力量執行保護海洋工作，幫助海洋逐步回復美麗健康，守護海洋生態。
       </Text>
 
       <Text as="p" {...paragraphProps}>
-      請即伸出援手，捐助綠色和平環境工作。
+        向美麗大海伸出援手，捐助綠色和平海洋工作。
       </Text>
-
-
-      <Heading
-        textAlign="center"
-        py="6"
-        {...headingProps}
-        color={`theme.${themeInterests}`}
-      >
-        常見問題
-      </Heading>
-
-      <DonateFAQ locale="HKChinese" />
     </>
   );
 };
