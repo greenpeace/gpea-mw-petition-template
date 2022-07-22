387431203ece0e1cc3deee79b8246d1b4a7fff2c
Chris Chan <chris.chan@greenpeace.org>
2022-07-22 15:53:04 +0800

    refactor hk dpt policy with parameters

--- apps/hk/dpt-policy/index.js
+++ apps/hk/dpt-policy/index.js
@@ -1,34 +1,36 @@
 import React, { useEffect, useRef } from 'react';
-import dynamic from 'next/dynamic';
+import { useSelector, useDispatch } from 'react-redux';
+import * as formActions from 'store/actions/action-types/form-actions';
+// Import library
+import { useInView } from 'react-intersection-observer';
+import { Box, Flex } from '@chakra-ui/react';
+// Import custom containers
+import PageContainer from '@containers/pageContainer';
 import OverflowWrapper from '@containers/overflowWrapper';
 import ContentContainer from '@containers/contentContainer';
 import FormContainer from '@containers/formContainer';
 import PetitionFooter from '@containers/petitionFooter';
-import { useInView } from 'react-intersection-observer';
-import { connect } from 'react-redux';
-import { Box, Flex } from '@chakra-ui/react';
+// Import custom components
+import HeroBanner from '@components/ResponsiveBanner/hero';
+import ThanksBanner from '@components/ResponsiveBanner/thanks';
+import DonationModule from '@components/GP/DonationModule';
+import SignupForm from '@components/GP/WebinarForm';
 import ScrollToTargetButton from '@components/ScrollToTargetButton/ScrollToTargetButton';
-
-import formContent from './form';
-import SEO from './SEO';
-
-import * as formActions from 'store/actions/action-types/form-actions';
-
-import heroBannerImage from './images/202207-plastic-dpt-policy-banner.jpg';
-
-const HeroBanner = dynamic(() => import('@components/ResponsiveBanner/hero'));
-const ThanksBanner = dynamic(() =>
-  import('@components/ResponsiveBanner/thanks'),
-);
 
 import Content from './Content';
 import Thankyou from './Thankyou';
-import PageContainer from '@containers/pageContainer';
-import DonationModule from '@components/GP/DonationModule';
-import SignupForm from '@components/GP/WebinarForm';
 
-function Index({ status, theme, setFormContent, signup }) {
-  const { submitted } = status;
+import formContent from './form';
+import SEO from './SEO';
+// Import static
+import heroBannerImage from './images/202207-plastic-dpt-policy-banner.jpg';
+
+function Index() {
+  const dispatch = useDispatch();
+  const signup = useSelector((state) => state?.signup);
+  const { step } = signup;
+  const submitted = useSelector((state) => state?.status?.submitted);
+  const theme = useSelector((state) => state?.theme);
   const { FirstName } = signup;
 
   let utmSource = new URLSearchParams(document.location.search).get(
@@ -41,53 +43,60 @@ function Index({ status, theme, setFormContent, signup }) {
   const mobileForm = useRef(null);
 
   useEffect(() => {
-    setFormContent(formContent);
-  }, []);
+    dispatch({ type: formActions.SET_FORM, data: formContent }); // set form content from form.json
+  }, [dispatch]);
 
   return (
     <>
       <SEO />
-      {submitted ? (
-        <ThanksBanner
-          bgImage={heroBannerImage}
-          content={{
-            title: `${
-              FirstName ? FirstName : '綠色和平支持者'
-            }，感謝您加入推動政府全面管制即棄餐具，訂立全面走塑時間表！`,
-            description: [''],
-          }}
-          // removeMask={true}
-          imageSrcset={[
-            {
-              media: '(min-width: 48em)',
-              srcset: heroBannerImage,
-            },
-            {
-              media: '',
-              srcset: heroBannerImage,
-            },
-          ]}
-        />
-      ) : (
-        <HeroBanner
-          bgImage={heroBannerImage}
-          content={{
-            title: '請即聯署<br/>要求政府於 22/23 年度立法落實管制即棄膠餐具',
-            description: [''],
-          }}
-          // removeMask={true}
-          imageSrcset={[
-            {
-              media: '(min-width: 48em)',
-              srcset: heroBannerImage,
-            },
-            {
-              media: '',
-              srcset: heroBannerImage,
-            },
-          ]}
-        />
-      )}
+      <Box>
+        {submitted ? (
+          <ThanksBanner
+            bgImage={theme?.params?.hero_image_desktop ?? heroBannerImage}
+            content={{
+              title: `${
+                FirstName ? FirstName : '綠色和平支持者'
+              }，感謝您加入推動政府全面管制即棄餐具，訂立全面走塑時間表！`,
+              description: [''],
+            }}
+            // removeMask={true}
+            imageSrcset={[
+              {
+                media: '(min-width: 48em)',
+                srcset: theme?.params?.hero_image_desktop ?? heroBannerImage,
+              },
+              {
+                media: '',
+                srcset: theme?.params?.hero_image_mobile ?? heroBannerImage,
+              },
+            ]}
+          />
+        ) : (
+          <HeroBanner
+            bgImage={theme?.params?.hero_image_desktop ?? heroBannerImage}
+            content={{
+              title:
+                `${
+                  theme?.params?.headline_prefix
+                    ? theme?.params?.headline_prefix + '<br/>'
+                    : ''
+                }` + '請即聯署<br/>要求政府於 22/23 年度立法落實管制即棄膠餐具',
+              description: [''],
+            }}
+            // removeMask={true}
+            imageSrcset={[
+              {
+                media: '(min-width: 48em)',
+                srcset: heroBannerImage,
+              },
+              {
+                media: '',
+                srcset: heroBannerImage,
+              },
+            ]}
+          />
+        )}
+      </Box>
       <PageContainer>
         <OverflowWrapper>
           <Flex flexDirection={{ base: 'column-reverse', md: 'row' }}>
@@ -132,16 +141,4 @@ function Index({ status, theme, setFormContent, signup }) {
   );
 }
 
-const mapStateToProps = ({ status, theme, signup }) => {
-  return { status, theme: theme.data, signup: signup.data };
-};
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    setFormContent: (data) => {
-      dispatch({ type: formActions.SET_FORM, data });
-    },
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Index);
+export default Index;
