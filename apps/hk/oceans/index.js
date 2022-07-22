1eb00ebbc1e3ec48e1ad5643c655675de5b4dd55
Chris Chan <chris.chan@greenpeace.org>
2022-07-22 12:24:00 +0800

    refactor multiple conditions render child components

--- apps/hk/oceans/index.js
+++ apps/hk/oceans/index.js
@@ -12,22 +12,22 @@ import formContent from './form';
 import SEO from './SEO';
 import * as formActions from 'store/actions/action-types/form-actions';
 
-import heroBannerImage from './images/GP1SUB1C_PressMedia_ed.jpg';
-
-import Donation from './Donation';
-import Content from './Content';
-import Thankyou from './Thankyou';
-
 import HeroBanner from '@components/Banner/hero';
 import ThanksBanner from '@components/Banner/thanks';
 import PageContainer from '@containers/pageContainer';
 import DonationModule from '@components/GP/DonationModule';
 import SignupForm from '@components/GP/HKForm';
+// Import Contents
+import Donation from './Donation';
+import Content from './Content';
+import Thankyou from './Thankyou';
+// Import Images
+import heroBannerImage from './images/GP1SUB1C_PressMedia_ed.jpg';
 
 function Index() {
   const dispatch = useDispatch();
   const signup = useSelector((state) => state?.signup);
-  const {step} = signup
+  const { step } = signup;
   const submitted = useSelector((state) => state?.status?.submitted);
   const theme = useSelector((state) => state?.theme);
 
@@ -43,19 +43,93 @@ function Index() {
   return (
     <>
       <SEO />
-      <HeroBannerCom step={step} submitted={submitted} theme={theme} />
+      <Box>
+        {(() => {
+          if (step === 'donation') {
+            return (
+              <HeroBanner
+                bgImage={theme?.params?.hero_image_desktop ?? heroBannerImage}
+                content={{
+                  title: `${
+                    theme?.params?.headline_prefix ?? ''
+                  }請即聯署<br/>將全球 30% 海洋<br/>納入保護區`,
+                  description: [''],
+                }}
+              />
+            );
+          } else {
+            return submitted ? (
+              <ThanksBanner
+                bgImage={theme?.params?.hero_image_desktop ?? heroBannerImage}
+                content={{
+                  title: `${
+                    signup?.data?.FirstName
+                      ? signup?.data?.FirstName
+                      : '綠色和平支持者'
+                  }，感謝您加入守護海洋行列！`,
+                  description: ['為海洋多走一步，捐助支持保護海洋項目。'],
+                }}
+              />
+            ) : (
+              <HeroBanner
+                bgImage={theme?.params?.hero_image_desktop ?? heroBannerImage}
+                content={{
+                  title: `${
+                    theme?.params?.headline_prefix ?? ''
+                  }請即聯署<br/>將全球 30% 海洋<br/>納入保護區`,
+                  description: [''],
+                }}
+              />
+            );
+          }
+        })()}
+      </Box>
       <PageContainer>
         <OverflowWrapper>
           <Flex flexDirection={{ base: 'column-reverse', md: 'row' }}>
             <Box flex={1} mt={{ base: 10, sm: 60 }}>
               <ContentContainer theme={theme}>
-                <ContentCom step={step} submitted={submitted} />
+                {(() => {
+                  if (step === 'donation') {
+                    return <Donation />;
+                  } else {
+                    return submitted ? <Thankyou /> : <Content />;
+                  }
+                })()}
               </ContentContainer>
             </Box>
             <Box flex={1} ref={mobileForm}>
               <FormContainer>
                 <Box ref={ref}>
-                  <FormCom step={step} submitted={submitted} theme={theme} />
+                  {(() => {
+                    if (step === 'donation') {
+                      return (
+                        <DonationModule
+                          market={'HK'}
+                          language={'zh_HK'}
+                          campaign={
+                            theme?.params?.donation_module_campaign ?? 'oceans'
+                          }
+                          // campaignId={''}
+                          env={'production'}
+                        />
+                      );
+                    } else {
+                      return submitted ? (
+                        <DonationModule
+                          market={'HK'}
+                          language={'zh_HK'}
+                          campaign={
+                            theme?.params?.donation_module_campaign ?? 'oceans'
+                          }
+                          // campaignId={''}
+                          env={'production'}
+                        />
+                      ) : (
+                        <SignupForm />
+                      );
+                    }
+                  })()}
                 </Box>
               </FormContainer>
             </Box>
@@ -67,75 +141,5 @@ function Index() {
     </>
   );
 }
-
-const FormCom = ({ step, submitted, theme }) => {
-  if (step === 'donation') {
-    return (
-      <DonationModule
-        market={theme.Market}
-        language={'zh_HK'}
-        campaign={'oceans'}
-        // campaignId={''}
-        env={'production'}
-      />
-    );
-  }
-
-  return submitted ? (
-    <DonationModule
-      market={theme.Market}
-      language={'zh_HK'}
-      campaign={'oceans'}
-      // campaignId={''}
-      env={'production'}
-    />
-  ) : (
-    <SignupForm />
-  );
-};
-
-const ContentCom = ({ step, submitted }) => {
-  if (step === 'donation') {
-    return <Donation />;
-  }
-
-  return submitted ? <Thankyou /> : <Content />;
-};
-
-const HeroBannerCom = ({ step, submitted, theme, signup }) => {
-  if (step === 'donation') {
-    return (
-      <HeroBanner
-        bgImage={theme?.params?.hero_image_desktop ?? heroBannerImage}
-        content={{
-          title: `${
-            theme?.params?.headline_prefix ?? ''
-          }請即聯署<br/>將全球 30% 海洋<br/>納入保護區`,
-          description: [''],
-        }}
-      />
-    );
-  }
-
-  return submitted ? (
-    <ThanksBanner
-      bgImage={heroBannerImage}
-      content={{
-        title: `${
-          signup?.data?.FirstName ? signup?.data?.FirstName : '綠色和平支持者'
-        }，感謝您加入守護海洋行列！`,
-        description: ['為海洋多走一步，捐助支持保護海洋項目。'],
-      }}
-    />
-  ) : (
-    <HeroBanner
-      bgImage={heroBannerImage}
-      content={{
-        title: '請即捐款<br/>讓全球30%海洋<br/>納入保護區',
-        description: [''],
-      }}
-    />
-  );
-};
 
 export default Index;
