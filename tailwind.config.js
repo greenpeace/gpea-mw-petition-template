d653935a0d4ccaa38faf338571f6bfa61b3e8f63
Chris Chan <chris.chan@greenpeace.org>
2022-08-22 10:39:49 +0800

    Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,11 @@ module.exports = {
   content: [
     './apps/**/*.{js,ts,jsx,tsx}',
     './pages/**/*.{js,ts,jsx,tsx}',
+    './containers/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/aspect-ratio')],
 };
