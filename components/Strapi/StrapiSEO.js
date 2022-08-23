cc6e642eafe03f5065e0dd0dbac2141a1b5835dc
Chris Chan <chris.chan@greenpeace.org>
2022-08-23 14:53:31 +0800

    Create Strapi shared SEO component

---
+++ components/Strapi/StrapiSEO.js
@@ -0,0 +1,33 @@
+import Head from 'next/head';
+import Script from 'next/script';
+import { useSelector } from 'react-redux';
+const StrapiSEO = () => {
+  const strapi = useSelector((state) => state?.theme?.strapi);
+
+  return (
+    <Head>
+      {/* campaign dataLayer */}
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          var dataLayer = (window.dataLayer = window.dataLayer || []);
+          dataLayer.push({
+            gCampaign: ${strapi?.issue?.data?.attributes?.slug},
+            gBasket: ${strapi?.campaign},
+          });
+        `,
+        }}
+      />
+      {/* title */}
+      <title>{strapi?.seo?.metaTitle}</title>
+      <meta property="og:title" content={strapi?.seo?.metaTitle} />
+      <meta name="description" content={strapi?.seo?.metaDescription} />
+      <meta property="og:description" content={strapi?.seo?.metaDescription} />
+      <meta property="og:image" content={strapi?.seo?.metaImageURL} />
+      <meta name="keywords" content={strapi?.seo?.keywords} />
+    </Head>
+  );
+};
+
+export default StrapiSEO;
