ded64eadccf9053bee6b239034c071043923e980
Chris Chan <chris.chan@greenpeace.org>
2022-09-27 10:39:06 +0800

    Add removeMask prop with Strapi

--- apps/hkPreview/index.js
+++ apps/hkPreview/index.js
@@ -26,207 +26,210 @@ import formContent from './form';
 // Import static
 
 function Index({ submitted = false, strapi: strapiData }) {
-  const strapi = strapiData ?? useSelector((state) => state?.theme?.strapi);
-  const router = useRouter();
-  const dispatch = useDispatch();
-  const theme = useSelector((state) => state?.theme);
-  const pageType = strapi?.page_type?.data?.attributes?.name;
-  const [ref, inView] = useInView({
-    threshold: 0,
-  });
-  const [isLoaded, setIsLoaded] = useState(false);
-  const FormRef = useRef(null);
+	const strapi = strapiData ?? useSelector((state) => state?.theme?.strapi);
+	const router = useRouter();
+	const dispatch = useDispatch();
+	const theme = useSelector((state) => state?.theme);
+	const pageType = strapi?.page_type?.data?.attributes?.name;
+	const [ref, inView] = useInView({
+		threshold: 0
+	});
+	const [isLoaded, setIsLoaded] = useState(false);
+	const FormRef = useRef(null);
 
-  submitted = useSelector((state) => state?.status?.submitted);
+	submitted = useSelector((state) => state?.status?.submitted);
 
-  useEffect(() => {
-    dispatch({ type: formActions.SET_FORM, data: formContent }); // set form content from form.json
-    if (pageType !== undefined) {
-      setIsLoaded(true);
-    }
-  }, [dispatch, pageType]);
+	useEffect(() => {
+		dispatch({ type: formActions.SET_FORM, data: formContent }); // set form content from form.json
+		if (pageType !== undefined) {
+			setIsLoaded(true);
+		}
+	}, [dispatch, pageType]);
 
-  useEffect(async () => {
-    if (router?.isReady) {
-      const { preview } = router?.query;
-      if (preview) {
-        const endpoint = 'https://strapi.small-service.gpeastasia.org/api';
-        const res = await fetch(
-          `${endpoint}/pages?filters[market][slug]=hk&filters[campaign]=${preview}&populate=*`,
-        ).then((response) => response);
-        const themes = await res.json();
-        const theme = themes?.data[0] ?? {};
+	useEffect(async () => {
+		if (router?.isReady) {
+			const { preview } = router?.query;
+			if (preview) {
+				const endpoint = 'https://strapi.small-service.gpeastasia.org/api';
+				const res = await fetch(
+					`${endpoint}/pages?filters[market][slug]=hk&filters[campaign]=${preview}&populate=*`
+				).then((response) => response);
+				const themes = await res.json();
+				const theme = themes?.data[0] ?? {};
 
-        dispatch({
-          type: themeActions.SET_STRAPI_DATA,
-          data: theme?.attributes,
-        });
-      }
-    }
-  }, [router, submitted]);
+				dispatch({
+					type: themeActions.SET_STRAPI_DATA,
+					data: theme?.attributes
+				});
+			}
+		}
+	}, [router, submitted]);
 
-  return (
-    <>
-      <StrapiSEO strapi={strapi} />
-      <Box>
-        {(() => {
-          if (pageType?.toLowerCase() === 'donation') {
-            return (
-              <HeroBanner
-                defaultImage={
-                  theme?.params?.hero_image_desktop ||
-                  strapi?.contentHero?.desktopImageURL
-                }
-                imageSrcset={[
-                  {
-                    media: '(min-width: 48em)',
-                    srcset:
-                      theme?.params?.hero_image_desktop ||
-                      strapi?.contentHero?.desktopImageURL,
-                  },
-                  {
-                    media: '',
-                    srcset:
-                      theme?.params?.hero_image_mobile ||
-                      strapi?.contentHero?.mobileImageURL,
-                  },
-                ]}
-                content={{
-                  title: theme?.params?.headline_prefix
-                    ? theme?.params?.headline_prefix +
-                      '<br/>' +
-                      strapi?.contentHero?.richContent
-                    : strapi?.contentHero?.richContent,
-                  description: strapi?.contentHero?.richContentParagraph,
-                }}
-              />
-            );
-          } else {
-            return submitted ? (
-              <ThanksBanner
-                defaultImage={
-                  theme?.params?.hero_image_desktop ||
-                  strapi?.thankyouHero?.desktopImageURL
-                }
-                imageSrcset={[
-                  {
-                    media: '(min-width: 48em)',
-                    srcset:
-                      theme?.params?.hero_image_desktop ||
-                      strapi?.contentHero?.desktopImageURL,
-                  },
-                  {
-                    media: '',
-                    srcset:
-                      theme?.params?.hero_image_mobile ||
-                      strapi?.contentHero?.mobileImageURL,
-                  },
-                ]}
-                content={{
-                  title: strapi?.thankyouHero?.richContent,
-                  description: strapi?.contentHero?.richContentParagraph,
-                }}
-              />
-            ) : (
-              <HeroBanner
-                defaultImage={
-                  theme?.params?.hero_image_desktop ||
-                  strapi?.thankyouHero?.desktopImageURL
-                }
-                imageSrcset={[
-                  {
-                    media: '(min-width: 48em)',
-                    srcset:
-                      theme?.params?.hero_image_desktop ||
-                      strapi?.contentHero?.desktopImageURL,
-                  },
-                  {
-                    media: '',
-                    srcset:
-                      theme?.params?.hero_image_mobile ||
-                      strapi?.contentHero?.mobileImageURL,
-                  },
-                ]}
-                content={{
-                  title: theme?.params?.headline_prefix
-                    ? theme?.params?.headline_prefix +
-                      '<br/>' +
-                      strapi?.contentHero?.richContent
-                    : strapi?.contentHero?.richContent,
-                  description: strapi?.contentHero?.richContentParagraph,
-                }}
-              />
-            );
-          }
-        })()}
-      </Box>
-      <PageContainer>
-        <OverflowWrapper>
-          <Flex flexDirection={{ base: 'column-reverse', md: 'row' }}>
-            <Box flex={1} mt={{ base: 10, sm: 60 }}>
-              <ContentContainer issue={strapi?.issue?.data?.attributes?.slug}>
-                {(() => {
-                  if (pageType?.toLowerCase() === 'donation') {
-                    return (
-                      <StrapiDynamicBlocks
-                        blocks={'contentBlocks'}
-                        strapi={strapi}
-                      />
-                    );
-                  } else {
-                    return submitted ? (
-                      <StrapiDynamicBlocks
-                        blocks={'thankyouBlocks'}
-                        strapi={strapi}
-                      />
-                    ) : (
-                      <StrapiDynamicBlocks
-                        blocks={'contentBlocks'}
-                        strapi={strapi}
-                      />
-                    );
-                  }
-                })()}
-              </ContentContainer>
-            </Box>
-            {isLoaded && (
-              <Box
-                flex={1}
-                ref={FormRef}
-                {...{ d: isLoaded ? 'block' : 'none' }}
-              >
-                <FormContainer>
-                  <Box ref={ref}>
-                    {pageType?.toLowerCase() === 'donation' || submitted ? (
-                      <DonationModule
-                        market={
-                          strapi?.market?.data?.attributes?.market ===
-                          'Hong Kong'
-                            ? 'HK'
-                            : 'TW'
-                        }
-                        language={strapi?.donationModuleLanguage}
-                        campaign={
-                          theme?.params?.donation_module_campaign ??
-                          strapi?.donationModuleCampaign
-                        }
-                        campaignId={theme?.params?.campaignId ?? ''}
-                        env={strapi?.donationModuleEnv}
-                      />
-                    ) : (
-                      <SignupForm />
-                    )}
-                  </Box>
-                </FormContainer>
-              </Box>
-            )}
-          </Flex>
-        </OverflowWrapper>
-      </PageContainer>
-      <PetitionFooter locale={'HKChinese'} />
-      <StrapiFixedButton target={FormRef} targetInView={inView} />
-    </>
-  );
+	return (
+		<>
+			<StrapiSEO strapi={strapi} />
+			<Box>
+				{(() => {
+					if (pageType?.toLowerCase() === 'donation') {
+						return (
+							<HeroBanner
+								removeMask={strapi?.contentHero?.removeMask}
+								defaultImage={
+									theme?.params?.hero_image_desktop ||
+									strapi?.contentHero?.desktopImageURL
+								}
+								imageSrcset={[
+									{
+										media: '(min-width: 48em)',
+										srcset:
+											theme?.params?.hero_image_desktop ||
+											strapi?.contentHero?.desktopImageURL
+									},
+									{
+										media: '',
+										srcset:
+											theme?.params?.hero_image_mobile ||
+											strapi?.contentHero?.mobileImageURL
+									}
+								]}
+								content={{
+									title: theme?.params?.headline_prefix
+										? theme?.params?.headline_prefix +
+										  '<br/>' +
+										  strapi?.contentHero?.richContent
+										: strapi?.contentHero?.richContent,
+									description: strapi?.contentHero?.richContentParagraph
+								}}
+							/>
+						);
+					} else {
+						return submitted ? (
+							<ThanksBanner
+								removeMask={strapi?.contentHero?.removeMask}
+								defaultImage={
+									theme?.params?.hero_image_desktop ||
+									strapi?.thankyouHero?.desktopImageURL
+								}
+								imageSrcset={[
+									{
+										media: '(min-width: 48em)',
+										srcset:
+											theme?.params?.hero_image_desktop ||
+											strapi?.contentHero?.desktopImageURL
+									},
+									{
+										media: '',
+										srcset:
+											theme?.params?.hero_image_mobile ||
+											strapi?.contentHero?.mobileImageURL
+									}
+								]}
+								content={{
+									title: strapi?.thankyouHero?.richContent,
+									description: strapi?.contentHero?.richContentParagraph
+								}}
+							/>
+						) : (
+							<HeroBanner
+								removeMask={strapi?.contentHero?.removeMask}
+								defaultImage={
+									theme?.params?.hero_image_desktop ||
+									strapi?.thankyouHero?.desktopImageURL
+								}
+								imageSrcset={[
+									{
+										media: '(min-width: 48em)',
+										srcset:
+											theme?.params?.hero_image_desktop ||
+											strapi?.contentHero?.desktopImageURL
+									},
+									{
+										media: '',
+										srcset:
+											theme?.params?.hero_image_mobile ||
+											strapi?.contentHero?.mobileImageURL
+									}
+								]}
+								content={{
+									title: theme?.params?.headline_prefix
+										? theme?.params?.headline_prefix +
+										  '<br/>' +
+										  strapi?.contentHero?.richContent
+										: strapi?.contentHero?.richContent,
+									description: strapi?.contentHero?.richContentParagraph
+								}}
+							/>
+						);
+					}
+				})()}
+			</Box>
+			<PageContainer>
+				<OverflowWrapper>
+					<Flex flexDirection={{ base: 'column-reverse', md: 'row' }}>
+						<Box flex={1} mt={{ base: 10, sm: 60 }}>
+							<ContentContainer issue={strapi?.issue?.data?.attributes?.slug}>
+								{(() => {
+									if (pageType?.toLowerCase() === 'donation') {
+										return (
+											<StrapiDynamicBlocks
+												blocks={'contentBlocks'}
+												strapi={strapi}
+											/>
+										);
+									} else {
+										return submitted ? (
+											<StrapiDynamicBlocks
+												blocks={'thankyouBlocks'}
+												strapi={strapi}
+											/>
+										) : (
+											<StrapiDynamicBlocks
+												blocks={'contentBlocks'}
+												strapi={strapi}
+											/>
+										);
+									}
+								})()}
+							</ContentContainer>
+						</Box>
+						{isLoaded && (
+							<Box
+								flex={1}
+								ref={FormRef}
+								{...{ d: isLoaded ? 'block' : 'none' }}
+							>
+								<FormContainer>
+									<Box ref={ref}>
+										{pageType?.toLowerCase() === 'donation' || submitted ? (
+											<DonationModule
+												market={
+													strapi?.market?.data?.attributes?.market ===
+													'Hong Kong'
+														? 'HK'
+														: 'TW'
+												}
+												language={strapi?.donationModuleLanguage}
+												campaign={
+													theme?.params?.donation_module_campaign ??
+													strapi?.donationModuleCampaign
+												}
+												campaignId={theme?.params?.campaignId ?? ''}
+												env={strapi?.donationModuleEnv}
+											/>
+										) : (
+											<SignupForm />
+										)}
+									</Box>
+								</FormContainer>
+							</Box>
+						)}
+					</Flex>
+				</OverflowWrapper>
+			</PageContainer>
+			<PetitionFooter locale={'HKChinese'} />
+			<StrapiFixedButton target={FormRef} targetInView={inView} />
+		</>
+	);
 }
 
 export default Index;
