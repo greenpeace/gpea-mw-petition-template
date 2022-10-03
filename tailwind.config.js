d3daff16caf2d2cdf980d00d08e1a10de5ec4ec7
Chris Chan <chris.chan@greenpeace.org>
2022-10-03 12:40:47 +0800

    Apply postcss cssnano

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,19 @@
 module.exports = {
-  content: [
-    './apps/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './containers/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  daisyui: {
-    themes: [
-      { gpea: { primary: '#66CC00', secondary: '#FF6100' } },
-      'light',
-      'cupcake',
-    ],
-  },
-  plugins: [require('daisyui'), require('@tailwindcss/aspect-ratio')],
+	content: [
+		'./apps/**/*.{js,ts,jsx,tsx}',
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./containers/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}'
+	],
+	theme: {
+		extend: {}
+	},
+	plugins: [require('daisyui'), require('@tailwindcss/aspect-ratio')],
+	daisyui: {
+		themes: [
+			{ gpea: { primary: '#66CC00', secondary: '#FF6100' } },
+			'light',
+			'cupcake'
+		]
+	}
 };
