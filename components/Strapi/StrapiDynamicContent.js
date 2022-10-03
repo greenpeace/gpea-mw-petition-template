ba778f69d978c2b74b87704d57a97ba81555c72f
Chris Chan <chris.chan@greenpeace.org>
2022-10-03 12:40:58 +0800

    Missing array map id

--- components/Strapi/StrapiDynamicContent.js
+++ components/Strapi/StrapiDynamicContent.js
@@ -1,18 +1,15 @@
 import React from 'react';
 
 const StrapiDynamicBlocks = ({ blocks = 'contentBlocks', strapi }) => {
-  return (
-    <div className="strapi-content">
-      {strapi?.[blocks]?.map((d) => {
-        return (
-          <div
-            key={d.id}
-            dangerouslySetInnerHTML={{ __html: d?.richContent }}
-          />
-        );
-      })}
-    </div>
-  );
+	return (
+		<div className="strapi-content">
+			{strapi?.[blocks]?.map((d, i) => {
+				return (
+					<div key={i} dangerouslySetInnerHTML={{ __html: d?.richContent }} />
+				);
+			})}
+		</div>
+	);
 };
 
 export default StrapiDynamicBlocks;
