82d85e17b2c7ab8542967d0432edbc310d06edcc
Chris Chan <chris.chan@greenpeace.org>
2022-10-06 10:58:16 +0800

    Add FAQs

--- apps/twPreview/index.js
+++ apps/twPreview/index.js
@@ -5,7 +5,7 @@ import * as formActions from 'store/actions/action-types/form-actions';
 import * as themeActions from 'store/actions/action-types/theme-actions';
 // Import library
 import { useInView } from 'react-intersection-observer';
-import { Box, Flex } from '@chakra-ui/react';
+import { Box, Flex, Heading } from '@chakra-ui/react';
 // Import custom containers
 import PageContainer from '@containers/pageContainer';
 import OverflowWrapper from '@containers/overflowWrapper';
@@ -17,6 +17,7 @@ import HeroBanner from '@components/ResponsiveBanner/hero';
 import ThanksBanner from '@components/ResponsiveBanner/thanks';
 import DonationModule from '@components/GP/DonationModule';
 import SignupForm from '@components/GP/TWForm';
+import DonateFAQ from '@components/DonateFAQ';
 // Import Strapi content components
 import StrapiSEO from '@components/Strapi/StrapiSEO';
 import StrapiDynamicBlocks from '@components/Strapi/StrapiDynamicContent';
@@ -191,6 +192,10 @@ function Index({ submitted = false, strapi }) {
 										);
 									}
 								})()}
+								<Heading textAlign="center" py="6" fontSize="2xl">
+									常見問題
+								</Heading>
+								<DonateFAQ locale="TWChinese" />
 							</ContentContainer>
 						</Box>
 						{isLoaded && (
