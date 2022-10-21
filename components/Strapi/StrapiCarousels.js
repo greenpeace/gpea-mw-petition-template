7b8e6f72db250fb839faae763212d4294e374978
Chris Chan <chris.chan@greenpeace.org>
2022-10-21 14:23:06 +0800

    Update Swiper components

---
+++ components/Strapi/StrapiCarousels.js
@@ -0,0 +1,196 @@
+import React from 'react';
+import { Box, Text, Stack } from '@chakra-ui/react';
+
+// Core modules imports are same as usual
+import {
+	EffectFade,
+	Mousewheel,
+	Autoplay,
+	Pagination,
+	Navigation
+} from 'swiper';
+// Direct React component imports
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-fade';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+export const SwiperCarousel = ({ data }) => {
+	const defaultConfig = {
+		spaceBetween: 20,
+		centeredSlides: true,
+		slidesPerView: 'auto',
+		loop: true,
+		autoplay: false,
+		pagination: {
+			clickable: true,
+			renderBullet: function (index, className) {
+				return '<span className="' + className + '"></span>';
+			}
+		}
+	};
+	return (
+		<Swiper
+			modules={[Autoplay, Mousewheel, Navigation, Pagination]}
+			{...defaultConfig}
+		>
+			<Box className="swiper-pagination" />
+			{data?.map((d, i) => (
+				<SwiperSlide key={i}>
+					<Box>
+						<Box
+							overflow={'hidden'}
+							w={'100%'}
+							h={{ base: '240px', md: '320px' }}
+							position="relative"
+						>
+							<Box
+								bgPosition={'center'}
+								objectFit={'cover'}
+								bgImage={d?.image?.data?.attributes?.url}
+								w={'100%'}
+								h={'100%'}
+								position={'absolute'}
+							/>
+						</Box>
+						<Box pt={2}>
+							<Text>{d?.title}</Text>
+							<Text fontSize={14} py={4}>
+								{d?.text}
+							</Text>
+						</Box>
+					</Box>
+				</SwiperSlide>
+			))}
+		</Swiper>
+	);
+};
+
+export const SingleCarousel = ({ data }) => {
+	const defaultConfig = {
+		slidesPerView: '1',
+		loop: true,
+		effect: 'fade',
+		fadeEffect: {
+			crossFade: true
+		},
+		autoplay: false,
+		pagination: {
+			clickable: true
+		}
+	};
+	return (
+		<Swiper modules={[EffectFade, Pagination]} {...defaultConfig}>
+			<Box className="swiper-pagination" /> {/** CSS */}
+			{data?.map((d, i) => (
+				<SwiperSlide key={i}>
+					<Box>
+						<Box
+							overflow={'hidden'}
+							w={'100%'}
+							h={{ base: '300px', md: '400px' }}
+							position="relative"
+						>
+							<Box
+								bgPosition={'center'}
+								objectFit={'cover'}
+								bgImage={d?.image?.data?.attributes?.url}
+								w={'100%'}
+								h={'100%'}
+								position={'absolute'}
+							/>
+						</Box>
+						<Box pt={2}>
+							<Text>{d?.title}</Text>
+							<Text fontSize={14} py={4}>
+								{d?.text}
+							</Text>
+						</Box>
+					</Box>
+				</SwiperSlide>
+			))}
+		</Swiper>
+	);
+};
+
+export const CardCarousel = ({ data, cardSide = false }) => {
+	const defaultConfig = {
+		spaceBetween: 20,
+		centeredSlides: true,
+		slidesPerView: 'auto',
+		loop: true,
+		autoplay: false,
+		pagination: {
+			clickable: true,
+			renderBullet: function (index, className) {
+				return '<span className="' + className + '"></span>';
+			}
+		}
+	};
+
+	return (
+		<Swiper modules={[Navigation, Pagination]} {...defaultConfig}>
+			{data?.map((d, i) => (
+				<SwiperSlide key={i}>
+					<div
+						className={`card mx-4 mb-10 bg-base-100 shadow-xl ${
+							cardSide && 'card-side w-full'
+						}`}
+					>
+						<figure>
+							<img src={d?.image?.data?.attributes?.url} alt={d?.title} />
+						</figure>
+						<div className="card-body">
+							<p className="card-title">{d?.title}</p>
+							<p className="">{d?.text}</p>
+						</div>
+					</div>
+				</SwiperSlide>
+			))}
+			<div className="swiper-pagination"></div>
+		</Swiper>
+	);
+};
+
+export const TestimonialCarousel = ({ data }) => {
+	const defaultConfig = {
+		spaceBetween: 20,
+		centeredSlides: true,
+		slidesPerView: 'auto',
+		loop: true,
+		autoplay: false,
+		pagination: {
+			clickable: true,
+			renderBullet: function (index, className) {
+				return '<span className="' + className + '"></span>';
+			}
+		}
+	};
+
+	return (
+		<Swiper modules={[Navigation, Pagination]} {...defaultConfig}>
+			<Box className="swiper-pagination" /> {/** CSS */}
+			{data?.map((d, i) => (
+				<SwiperSlide key={i}>
+					<Stack
+						direction="column"
+						textAlign="center"
+						space="4"
+						alignItems={'center'}
+					>
+						<div className="avatar mt-2">
+							<div className="w-24 rounded-full ring ring-primary ring-offset-2 ring-offset-base-100">
+								<img src={d?.avatar?.data?.attributes?.url} alt={d?.name} />
+							</div>
+						</div>
+						<Text className="">{d?.name}</Text>
+						<Box className="" dangerouslySetInnerHTML={{ __html: d?.quote }} />
+					</Stack>
+				</SwiperSlide>
+			))}
+		</Swiper>
+	);
+};
--- components/Strapi/StrapiDynamicContent.js
+++ components/Strapi/StrapiDynamicContent.js
@@ -1,210 +1,17 @@
 import React from 'react';
 import { Box, Heading, Text, Stack } from '@chakra-ui/react';
-import { Mousewheel, Autoplay, Pagination, Navigation } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react';
+
+import {
+	SwiperCarousel,
+	SingleCarousel,
+	CardCarousel,
+	TestimonialCarousel
+} from './StrapiCarousels';
 
 import {
 	headingProps,
 	paragraphProps
 } from '@common/styles/components/contentStyle';
-// Import Swiper styles
-import 'swiper/css/bundle';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-
-const SwiperCarousel = ({ data }) => {
-	const defaultConfig = {
-		spaceBetween: 20,
-		centeredSlides: true,
-		slidesPerView: 'auto',
-		loop: true,
-		fadeEffect: { crossFade: true },
-		autoplay: false,
-		pagination: {
-			clickable: true,
-			renderBullet: function (index, className) {
-				return '<span className="' + className + '"></span>';
-			}
-		}
-	};
-	return (
-		<Swiper
-			className="slider"
-			modules={[Autoplay, Mousewheel, Navigation, Pagination]}
-			{...defaultConfig}
-		>
-			<Box className="swiper-pagination" />
-			{data?.map((d, i) => (
-				<SwiperSlide key={i}>
-					<Box>
-						<Box
-							overflow={'hidden'}
-							w={'100%'}
-							h={{ base: '240px', md: '320px' }}
-							position="relative"
-						>
-							<Box
-								bgPosition={'center'}
-								objectFit={'cover'}
-								bgImage={d?.image?.data?.attributes?.url}
-								w={'100%'}
-								h={'100%'}
-								position={'absolute'}
-							/>
-						</Box>
-						<Box pt={2}>
-							<Text>{d?.title}</Text>
-							<Text fontSize={14} py={4}>
-								{d?.text}
-							</Text>
-						</Box>
-					</Box>
-				</SwiperSlide>
-			))}
-		</Swiper>
-	);
-};
-
-const SingleCarousel = ({ data }) => {
-	const defaultConfig = {
-		slidesPerView: '1',
-		loop: true,
-		fadeEffect: { crossFade: true },
-		autoplay: false,
-		pagination: {
-			clickable: true,
-			renderBullet: function (index, className) {
-				return '<span className="' + className + '"></span>';
-			}
-		}
-	};
-	return (
-		<Swiper
-			className="slider"
-			modules={[Autoplay, Mousewheel, Navigation, Pagination]}
-			{...defaultConfig}
-		>
-			<Box className="swiper-pagination" /> {/** CSS */}
-			{data?.map((d, i) => (
-				<SwiperSlide key={i} className="dynamic-swiper">
-					<Box>
-						<Box
-							overflow={'hidden'}
-							w={'100%'}
-							h={{ base: '240px', md: '320px' }}
-							position="relative"
-						>
-							<Box
-								bgPosition={'center'}
-								objectFit={'cover'}
-								bgImage={d?.image?.data?.attributes?.url}
-								w={'100%'}
-								h={'100%'}
-								position={'absolute'}
-							/>
-						</Box>
-						<Box pt={2}>
-							<Text>{d?.title}</Text>
-							<Text fontSize={14} py={4}>
-								{d?.text}
-							</Text>
-						</Box>
-					</Box>
-				</SwiperSlide>
-			))}
-		</Swiper>
-	);
-};
-
-const CardCarousel = ({ data, cardSide = false }) => {
-	const defaultConfig = {
-		spaceBetween: 20,
-		centeredSlides: true,
-		slidesPerView: 'auto',
-		loop: true,
-		fadeEffect: { crossFade: true },
-		autoplay: false,
-		pagination: {
-			clickable: true,
-			renderBullet: function (index, className) {
-				return '<span className="' + className + '"></span>';
-			}
-		}
-	};
-
-	return (
-		<Swiper
-			className="slider"
-			modules={[Autoplay, Mousewheel, Navigation, Pagination]}
-			{...defaultConfig}
-		>
-			{data?.map((d, i) => (
-				<SwiperSlide key={i}>
-					<div
-						className={`card mx-4 mb-10 bg-base-100 shadow-xl ${
-							cardSide && 'card-side w-full'
-						}`}
-					>
-						<figure>
-							<img src={d?.image?.data?.attributes?.url} alt={d?.title} />
-						</figure>
-						<div className="card-body">
-							<p className="card-title">{d?.title}</p>
-							<p className="">{d?.text}</p>
-						</div>
-					</div>
-				</SwiperSlide>
-			))}
-			<div className="swiper-pagination"></div>
-		</Swiper>
-	);
-};
-
-const TestimonialSwiperCarousel = ({ data }) => {
-	const defaultConfig = {
-		spaceBetween: 20,
-		centeredSlides: true,
-		slidesPerView: 'auto',
-		loop: true,
-		fadeEffect: { crossFade: true },
-		autoplay: false,
-		pagination: {
-			clickable: true,
-			renderBullet: function (index, className) {
-				return '<span className="' + className + '"></span>';
-			}
-		}
-	};
-
-	return (
-		<Swiper
-			className="slider"
-			modules={[Autoplay, Mousewheel, Navigation, Pagination]}
-			{...defaultConfig}
-		>
-			<Box className="swiper-pagination" /> {/** CSS */}
-			{data?.map((d, i) => (
-				<SwiperSlide key={i}>
-					<Stack
-						direction="column"
-						textAlign="center"
-						space="4"
-						alignItems={'center'}
-					>
-						<div className="avatar mt-2">
-							<div className="w-24 rounded-full ring ring-primary ring-offset-2 ring-offset-base-100">
-								<img src={d?.avatar?.data?.attributes?.url} alt={d?.name} />
-							</div>
-						</div>
-						<Text className="">{d?.name}</Text>
-						<Box className="" dangerouslySetInnerHTML={{ __html: d?.quote }} />
-					</Stack>
-				</SwiperSlide>
-			))}
-		</Swiper>
-	);
-};
 
 // Dynamic Content
 const StrapiDynamicBlocks = ({ content }) => {
@@ -243,7 +50,7 @@ const StrapiDynamicBlocks = ({ content }) => {
 					<Text as="p" {...paragraphProps}>
 						{content?.text}
 					</Text>
-					<TestimonialSwiperCarousel data={content?.TestimonialSlider} />
+					<TestimonialCarousel data={content?.TestimonialSlider} />
 				</Box>
 			);
 	}
