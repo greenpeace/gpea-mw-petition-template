038e9e323e040d9719fa215e978a6d3b80fd6b02
Chris Chan <chris.chan@greenpeace.org>
2022-10-24 13:52:30 +0800

    To be fixed Hide broken carousel components

--- components/Strapi/StrapiDynamicContent.js
+++ components/Strapi/StrapiDynamicContent.js
@@ -14,46 +14,62 @@ import {
 } from '@common/styles/components/contentStyle';
 
 // Dynamic Content
-const StrapiDynamicBlocks = ({ content }) => {
-	switch (content?.__component) {
-		case 'blocks.rich-content':
-			return (
-				<Box py="4">
-					<div className="strapi-content">
-						<div dangerouslySetInnerHTML={{ __html: content?.richContent }} />
-					</div>
-				</Box>
-			);
-		case 'blocks.card-swiper':
-			return (
-				<Box py="4">
-					<Stack direction="column" textAlign="center" py={2}>
-						<Heading {...headingProps}>{content?.title}</Heading>
-						<Text as="p" {...paragraphProps}>
-							{content?.text}
-						</Text>
-					</Stack>
-					<Text>Default Swiper</Text>
-					<SwiperCarousel data={content?.CardSlider} />
-					<Text>Single Swiper</Text>
-					<SingleCarousel data={content?.CardSlider} />
-					<Text>Card Swiper</Text>
-					<CardCarousel data={content?.CardSlider} />
-					<Text>Card Swiper</Text>
-					<CardCarousel data={content?.CardSlider} cardSide={true} />
-				</Box>
-			);
-		case 'blocks.testimonial-swiper':
-			return (
-				<Box py="4">
-					<Heading {...headingProps}>{content?.title}</Heading>
-					<Text as="p" {...paragraphProps}>
-						{content?.text}
-					</Text>
-					<TestimonialCarousel data={content?.TestimonialSlider} />
-				</Box>
-			);
+const StrapiDynamicBlocks = ({ blocks = 'contentBlocks', strapi }) => {
+	if (!strapi) {
+		return null;
 	}
+
+	return strapi?.[blocks]?.map((content, i) => {
+		return (
+			<div key={i}>
+				{(() => {
+					switch (content?.__component) {
+						case 'blocks.rich-content':
+							return (
+								<Box py="4">
+									<div className="strapi-content">
+										<div
+											dangerouslySetInnerHTML={{ __html: content?.richContent }}
+										/>
+									</div>
+								</Box>
+							);
+						// case 'blocks.card-swiper':
+						// 	return (
+						// 		<Box py="4">
+						// 			<Stack direction="column" textAlign="center" py={2}>
+						// 				<Heading {...headingProps}>{content?.title}</Heading>
+						// 				<Text as="p" {...paragraphProps}>
+						// 					{content?.text}
+						// 				</Text>
+						// 			</Stack>
+						// 			<Text>Default Swiper</Text>
+						// 			<SwiperCarousel data={content?.CardSlider} />
+						// 			<Text>Single Swiper</Text>
+						// 			<SingleCarousel data={content?.CardSlider} />
+						// 			<Text>Card Swiper</Text>
+						// 			<CardCarousel data={content?.CardSlider} />
+						// 			<Text>Card Swiper</Text>
+						// 			<CardCarousel data={content?.CardSlider} cardSide={true} />
+						// 		</Box>
+						// 	);
+						// case 'blocks.testimonial-swiper':
+						// 	return (
+						// 		<Box py="4">
+						// 			<Heading {...headingProps}>{content?.title}</Heading>
+						// 			<Text as="p" {...paragraphProps}>
+						// 				{content?.text}
+						// 			</Text>
+						// 			<TestimonialCarousel data={content?.TestimonialSlider} />
+						// 		</Box>
+						// 	);
+						default:
+							return null;
+					}
+				})()}
+			</div>
+		);
+	});
 };
 
 export default StrapiDynamicBlocks;
