c8c03bf6670484fb0ad82f5573c57516fa8ebfd8
Chris Chan <chris.chan@greenpeace.org>
2022-10-25 11:24:41 +0800

    Show carousel components

--- components/Strapi/StrapiDynamicContent.js
+++ components/Strapi/StrapiDynamicContent.js
@@ -34,35 +34,35 @@ const StrapiDynamicBlocks = ({ blocks = 'contentBlocks', strapi }) => {
 									</div>
 								</Box>
 							);
-						// case 'blocks.card-swiper':
-						// 	return (
-						// 		<Box py="4">
-						// 			<Stack direction="column" textAlign="center" py={2}>
-						// 				<Heading {...headingProps}>{content?.title}</Heading>
-						// 				<Text as="p" {...paragraphProps}>
-						// 					{content?.text}
-						// 				</Text>
-						// 			</Stack>
-						// 			<Text>Default Swiper</Text>
-						// 			<SwiperCarousel data={content?.CardSlider} />
-						// 			<Text>Single Swiper</Text>
-						// 			<SingleCarousel data={content?.CardSlider} />
-						// 			<Text>Card Swiper</Text>
-						// 			<CardCarousel data={content?.CardSlider} />
-						// 			<Text>Card Swiper</Text>
-						// 			<CardCarousel data={content?.CardSlider} cardSide={true} />
-						// 		</Box>
-						// 	);
-						// case 'blocks.testimonial-swiper':
-						// 	return (
-						// 		<Box py="4">
-						// 			<Heading {...headingProps}>{content?.title}</Heading>
-						// 			<Text as="p" {...paragraphProps}>
-						// 				{content?.text}
-						// 			</Text>
-						// 			<TestimonialCarousel data={content?.TestimonialSlider} />
-						// 		</Box>
-						// 	);
+						case 'blocks.card-swiper':
+							return (
+								<Box py="4">
+									<Stack direction="column" textAlign="center" py={2}>
+										<Heading {...headingProps}>{content?.title}</Heading>
+										<Text as="p" {...paragraphProps}>
+											{content?.text}
+										</Text>
+									</Stack>
+									{/* <Text>Default Swiper</Text>
+									<SwiperCarousel data={content?.CardSlider} />
+									<Text>Single Swiper</Text>
+									<SingleCarousel data={content?.CardSlider} /> */}
+									<Text>Card Swiper</Text>
+									<CardCarousel data={content?.CardSlider} />
+									<Text>Card Swiper</Text>
+									<CardCarousel data={content?.CardSlider} cardSide={true} />
+								</Box>
+							);
+						case 'blocks.testimonial-swiper':
+							return (
+								<Box py="4">
+									<Heading {...headingProps}>{content?.title}</Heading>
+									<Text as="p" {...paragraphProps}>
+										{content?.text}
+									</Text>
+									<TestimonialCarousel data={content?.TestimonialSlider} />
+								</Box>
+							);
 						default:
 							return null;
 					}
