ec20c5d690fa6492fff71e162fda83fc4a9efaa8
KFC <chris.chan@greenpeace.org>
2022-10-28 12:18:32 +0800

    Revert local FAQ component

--- components/DonateFAQ/index.js
+++ components/DonateFAQ/index.js
@@ -1,53 +1,49 @@
 import React from 'react';
 import {
-  Accordion,
-  AccordionItem,
-  AccordionButton,
-  AccordionPanel,
-  AccordionIcon,
-  Box,
-  Text,
+	Accordion,
+	AccordionItem,
+	AccordionButton,
+	AccordionPanel,
+	AccordionIcon,
+	Box,
+	Text
 } from '@chakra-ui/react';
 
-import {
-  paragraphProps,
-} from '@common/styles/components/contentStyle';
+import { paragraphProps } from '@common/styles/components/contentStyle';
 
 import data from './index.json';
 
-const DonateFAQ = ({ locale = 'HKChinese', faq }) => {
-  const list = faq?.attributes?.questionAnswer??data[locale]?.accordions
-
-  return (
-    <Accordion my="4" allowToggle={true}>
-      {list?.map((d, i) => {
-        return (
-          <AccordionItem key={i}>
-            <AccordionButton
-              _expanded={{ fontWeight: 'bold', bg: '', color: '' }}
-            >
-              <AccordionIcon mr="2" />
-              <Box py="6" p="4" flex="1" textAlign="left">
-                <Text as="p" fontSize="md">
-                  {d.question??d.title}
-                </Text>
-              </Box>
-            </AccordionButton>
-            <AccordionPanel p="4">
-              <Text
-                as="p"
-                {...paragraphProps}
-                textAlign={'initial'}
-                dangerouslySetInnerHTML={{
-                  __html: d.answer??d.desc,
-                }}
-              ></Text>
-            </AccordionPanel>
-          </AccordionItem>
-        );
-      })}
-    </Accordion>
-  );
+const DonateFAQ = ({ locale = 'HKChinese' }) => {
+	return (
+		<Accordion my="4" allowToggle={true}>
+			{data[locale].accordions.map((d, i) => {
+				return (
+					<AccordionItem key={i}>
+						<AccordionButton
+							_expanded={{ fontWeight: 'bold', bg: '', color: '' }}
+						>
+							<AccordionIcon mr="2" />
+							<Box py="6" p="4" flex="1" textAlign="left">
+								<Text as="p" fontSize="md">
+									{d.title}
+								</Text>
+							</Box>
+						</AccordionButton>
+						<AccordionPanel p="4">
+							<Text
+								as="p"
+								{...paragraphProps}
+								textAlign={'initial'}
+								dangerouslySetInnerHTML={{
+									__html: d.desc
+								}}
+							></Text>
+						</AccordionPanel>
+					</AccordionItem>
+				);
+			})}
+		</Accordion>
+	);
 };
 
 export default DonateFAQ;
