c05599c37c76146da5af565274a4a35ee36964c3
bi-jen <bijen.chen@greenpeace.org>
2023-04-03 10:32:35 +0800

    set object position of hk petition-plastics-earthday-2023quiz landing

--- apps/hk/petition-plastics-earthday-2023quiz/landing.js
+++ apps/hk/petition-plastics-earthday-2023quiz/landing.js
@@ -4,11 +4,8 @@ import LazyShow from './components/LazyShow';
 import * as surveyActions from 'store/actions/action-types/survey-actions';
 
 import bg from './images/earthday-landing-bg.png';
-import mobileBG from './images/earthday-landing-bg-m.png';
+import mobileBG from './images/earthday-landing-bg-m.jpg';
 import TitleKeyVisual from './images/earthday-landing-title.png';
-// import signPicture from './images/earthday-sign.png';
-// import signPictureMobile from './images/earthday-sign-m.png';
-import startButton from './images/earthday-sign-m.png';
 
 
 const Landing = ({ setSurveyPage }) => {
@@ -21,8 +18,7 @@ const Landing = ({ setSurveyPage }) => {
 					w={{ base: '50%', md: '48%', xl: '50%' }}
 					minWidth={'180px'}
 					alignItems={{ base: 'flex-start', md: 'center' }}
-					// ml={{ xl: '25%' }}
-					ml={{ xl: '6%' }}
+					ml={{ xl: '25%' }}
 				>
 					<Stack direction="column">
 						<Box
@@ -31,8 +27,7 @@ const Landing = ({ setSurveyPage }) => {
 							w={{ base: 'auto', md: 'auto' }}
 							maxWidth={{ base: '360px', '2xl': '420px' }}
 							pt={{ base: 20, md: 6, lg: 0 }}
-							// mt={{ base: '5vh', '2xl': '5vh' }}
-							mt={{ base: '5vh', '2xl': '-5vh' }}
+							mt={{ base: '5vh', '2xl': '5vh' }}
 						>
 							<LazyShow initial={{ opacity: 0, x: 0, y: 0 }} duration={0.25}>
 								<Image
@@ -49,8 +44,7 @@ const Landing = ({ setSurveyPage }) => {
 						>
 							<LazyShow initial={{ opacity: 0, x: 0, y: 0 }} duration={0.25}>
 								<Button
-									// mt={1}
-									mt={4}
+									mt={1}
 									ml={{ base: 6, md: 4 }}
 									variant={'quizSquare'}
 									fontSize={{ base: 'xl', sm: '2xl' }}
@@ -62,33 +56,6 @@ const Landing = ({ setSurveyPage }) => {
 								</Button>
 							</LazyShow>
 						</Box>
-						{/* <Box>
-							<Image
-								d={{ base: 'block', md: 'none' }}
-								// position="absolute"
-								// bottom={{base: '10%', md:'1%', lg: '2%', xl: '2%'}}
-								left={{ base: 0 }}
-								mt={'8vh'}
-								ml={7}
-								src={signPictureMobile}
-								w={{ base: '30%', sm: '40%' }}
-								minWidth={'100px'}
-							/>
-						</Box> */}
-
-						{/* <Box
-								maxWidth={{ base: '50%', sm: 'auto' }}
-								alignSelf={'center'}
-								onClick={() => setSurveyPage('description')}
-							>
-								<LazyShow initial={{ opacity: 0, x: 0, y: 0 }} duration={0.25}>
-									<Image
-									src={startButton}
-									cursor={'pointer'}
-									_hover={{ opacity: 0.85 }}
-									/>
-								</LazyShow>
-							</Box> */}
 					</Stack>
 				</Center>
 			</Box>
@@ -110,23 +77,6 @@ const Landing = ({ setSurveyPage }) => {
 				}}
 				zIndex={'1'}
 			>
-				{/* <Image
-					d={{ base: 'none', lg: 'block' }}
-					position="absolute"
-					bottom={{ lg: '3%', xl: '2%' }}
-					left={{ lg: '78%', xl: '70%' }}
-					src={signPicture}
-					w={{ base: 'auto', md: '18%', lg: '10%' }}
-				/>
-
-				<Image
-					d={{ base: 'none', md: 'block', lg: 'none' }}
-					position="absolute"
-					bottom={{ base: '0', md: '5%' }}
-					left={{ base: '0', md: '20%' }}
-					src={signPictureMobile}
-					w={{ base: 'auto', md: '18%', lg: '12%' }}
-				/> */}
 			</Box>
 		</Box>
 	);
