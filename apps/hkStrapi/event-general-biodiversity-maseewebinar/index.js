d3e9333d27226e14232a28eb844f91769442e6df
ND-captu <captucaptu@nowilldesign.com>
2023-05-03 15:43:47 +0800

    add deployment setting comment for hk event biodiversity massewebiner

--- apps/hkStrapi/event-general-biodiversity-maseewebinar/index.js
+++ apps/hkStrapi/event-general-biodiversity-maseewebinar/index.js
@@ -1,3 +1,14 @@
+
+/**
+ * Deploy setting
+# Project Apps Directory: /apps/{PROJECT}
+PROJECT=hkStrapi/event-general-biodiversity-maseewebinar
+MARKET=hk
+PROJECT_NAME=event-general-biodiversity-maseewebinar
+BASEPATH=/web/api.greenpeace.org.hk/htdocs/page/event-general-biodiversity-maseewebinar
+ASSETPREFIX=https://api.greenpeace.org.hk/page/event-general-biodiversity-maseewebinar/
+FTP_CONFIG_NAME=api_hk_cloud 
+*/
 import React, { useEffect, useState, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useRouter } from 'next/router';
