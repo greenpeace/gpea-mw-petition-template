96ca1e74aca801ae9b479fa38c8119fd51a1536d
Nelson Liou <mhanto0712@gmail.com>
2023-08-02 15:12:58 +0800

    update HK petition-general-biodiversity_masee-ebook content

--- apps/hkStrapi/petition-general-biodiversity_masee-ebook/index.js
+++ apps/hkStrapi/petition-general-biodiversity_masee-ebook/index.js
@@ -7,6 +7,7 @@ PROJECT_NAME=petition-general-biodiversity_masee-ebook
 BASEPATH=/web/api.greenpeace.org.hk/htdocs/page/petition-general-biodiversity_masee-ebook
 ASSETPREFIX=https://api.greenpeace.org.hk/page/petition-general-biodiversity_masee-ebook/
 FTP_CONFIG_NAME=api_hk_cloud 
+CLOUD_PAGE_NAME=zh-hk.2023.general.webinar_biodiversity_masee.ebook.signup.na
 */
 
 import React, { useEffect, useState, useRef } from 'react';
@@ -51,18 +52,18 @@ function Index({ submitted = false, strapi: strapiData }) {
 	const FormRef = useRef(null);
 
 	// get utm_source
-  const hiddenForm = useSelector((state) => state?.hiddenForm);
-  const { utm_source, AsiaPayResult } = hiddenForm?.data;
+	const hiddenForm = useSelector((state) => state?.hiddenForm);
+	const { utm_source, AsiaPayResult } = hiddenForm?.data;
 
-  // pass signer / donor name to TY Banner
-  const [TYName, setTYName] = useState();
-	
+	// pass signer / donor name to TY Banner
+	const [TYName, setTYName] = useState();
+
 	useEffect(() => {
 		// get donation module firstname
 		window.__greenpeace__ = window.__greenpeace__ || {};
-		window.__greenpeace__.onDonationModulePaymentCompleted = function( data ) {
+		window.__greenpeace__.onDonationModulePaymentCompleted = function (data) {
 			setTYName(data.firstName);
-		}
+		};
 	});
 	useEffect(() => {
 		setTYName(signup?.data?.FirstName);
@@ -96,7 +97,6 @@ function Index({ submitted = false, strapi: strapiData }) {
 			}
 		}
 	}, [router]);
-	
 
 	return (
 		<>
@@ -127,9 +127,9 @@ function Index({ submitted = false, strapi: strapiData }) {
 								]}
 								content={{
 									// title: strapi?.thankyouHero?.richContent,
-									title: `${
-										TYName ? TYName : '綠色和平支持者'
-									}，${strapi?.thankyouHero?.richContent}`,
+									title: `${TYName ? TYName : '綠色和平支持者'}，${
+										strapi?.thankyouHero?.richContent
+									}`,
 									description: strapi?.thankyouHero?.richContentParagraph
 								}}
 							/>
@@ -157,8 +157,8 @@ function Index({ submitted = false, strapi: strapiData }) {
 								content={{
 									title: theme?.params?.headline_prefix
 										? theme?.params?.headline_prefix +
-										'<br/>' +
-										strapi?.contentHero?.richContent
+										  '<br/>' +
+										  strapi?.contentHero?.richContent
 										: strapi?.contentHero?.richContent,
 									description: strapi?.contentHero?.richContentParagraph
 								}}
@@ -210,11 +210,13 @@ function Index({ submitted = false, strapi: strapiData }) {
 							{isLoaded && (
 								<FormContainer>
 									<Box ref={ref}>
-										{pageType?.toLowerCase() === 'donation' || submitted || AsiaPayResult ? (
+										{pageType?.toLowerCase() === 'donation' ||
+										submitted ||
+										AsiaPayResult ? (
 											<DonationModule
 												market={
 													strapi?.market?.data?.attributes?.market ===
-														'Hong Kong'
+													'Hong Kong'
 														? 'HK'
 														: 'TW'
 												}
@@ -230,7 +232,9 @@ function Index({ submitted = false, strapi: strapiData }) {
 												}
 												isUAT={false}
 												env={strapi?.donationModuleEnv}
-												customUrl={'https://api.greenpeace.org.hk/app/donation-module-hkmp/main.js'}
+												customUrl={
+													'https://api.greenpeace.org.hk/app/donation-module-hkmp/main.js'
+												}
 											/>
 										) : (
 											<SignupForm />
