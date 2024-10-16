6cfef3e8665339455e627457e2dc31e6d1537426
wenting lin <wentinglin1206@gmail.com>
2024-10-16 15:12:52 +0800

    assetprefix 更改

--- apps/krStrapi/petition-plastic-yongginae/index.js
+++ apps/krStrapi/petition-plastic-yongginae/index.js
@@ -4,7 +4,7 @@ PROJECT=krStrapi/petition-plastic-yongginae
 MARKET=kr
 PROJECT_NAME=petition-plastic-yongginae
 BASEPATH=/web/gpseoulwebserver.co.kr/htdocs/petitions/petition-plastic-yongginae
-ASSETPREFIX=https://gpseoulwebserver.co.kr/petition/petition-plastic-yongginae/
+ASSETPREFIX=https://gpseoulwebserver.co.kr/petitions/petition-plastic-yongginae/
 FTP_CONFIG_NAME=ftp_kr
 # ******** MC Cloud Page Name ********
 CLOUD_PAGE_NAME=api-plastic-yongginae
